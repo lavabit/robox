641bd1b28500d30154476f7d2881127c9605be63
Ladar Levison <ladar@lavabit.com>
2016-06-15 22:13:31 -0500

    Add removal of the insall logs to the cleanup script.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -17,6 +17,9 @@ printf "Remove packages only required for provisioning purposes and then dump th
 yum --assumeyes remove dmidecode yum-utils
 yum --assumeyes clean all
 
+# Remove the installation logs.
+rm --force /root/anaconda-ks.cfg /root/install.log /root/install.log.syslog
+
 # Truncate the log files.
 printf "Truncate the log files.\n"
 find /var/log -type f -exec truncate --size=0 {} \;
