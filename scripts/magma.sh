dc945d22b31edd0547fef5db7b88fd074dea2536
Ladar Levison <ladar@lavabit.com>
2016-06-21 06:44:07 -0500

    Added logic to enable ClamAV and update the signatures.

--- scripts/magma.sh
+++ scripts/magma.sh
@@ -21,16 +21,26 @@ magma-develop/dev/scripts/linkup.sh
 chmod g=,o= magma-develop/sandbox/etc/localhost.localdomain.pem
 chmod g=,o= magma-develop/sandbox/etc/dkim.localhost.localdomain.pem
 
-# Compile the code.
+# Compile the dependencies into a shared library.
 build.lib all
+
+# Compile the daemon and then compile the unit tests.
 build.magma
 build.check
 
 # Reset the sandbox database and storage files.
 schema.reset
 
+# Enable the anti-virus engine and update the signatures.
+freshen.clamav 2>&1 | grep -v WARNING | grep -v PANIC
+sed -i -e "s/virus.available = false/virus.available = true/" magma-develop/sandbox/etc/magma.sandbox.config
+
 # Run the unit tests.
 check.run
+
+# Alternatively, run the unit tests atop Valgrind. 
+# Note this takes awhile when the anti-virus engine is enabled.
+# check.vg
 
 # Launch the daemon.
 (magma.run) &
