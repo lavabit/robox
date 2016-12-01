5bdb9fe4059e4965e1c19fbd4e8f10c8e4df7851
Ladar Levison <ladar@lavabit.com>
2016-12-01 17:54:50 -0600

    Missed a yum all in the previous commit.

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -10,7 +10,7 @@ error() {
 
 # Now that the system is running on the updated kernel, we can remove the 
 # old kernel(s) from the system.
-package-cleanup --oldkernels --count=1
+package-cleanup --quiet --assumeyes --oldkernels --count=1
 
 # Now that the system is running atop the updated kernel, we can install the 
 # development files for the kernel. These files are required to compile the 
