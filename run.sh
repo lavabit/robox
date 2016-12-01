0b85050037580f22980950092ec26328e8e6a39f
Ladar Levison <ladar@lavabit.com>
2016-12-01 07:23:35 -0600

    Added initial CentOS 7 template, plus misc bug fixes.

--- run.sh
+++ run.sh
@@ -5,17 +5,19 @@ BASE=`dirname $LINK`
 
 cd $BASE
 export VBOX_USER_HOME="$BASE"
-export ATLAS_TOKEN="qyToIsMKMP9P0w.atlasv1.MiyPtcThL0y4Fwk53lFri83nOEt1rUDSQNW2CxFbxJtFd7llvllpqSL176pTkeFVfiE"
+#export ATLAS_TOKEN="qyToIsMKMP9P0w.atlasv1.MiyPtcThL0y4Fwk53lFri83nOEt1rUDSQNW2CxFbxJtFd7llvllpqSL176pTkeFVfiE"
 
 #sudo /etc/init.d/vmware start
-#sudo systemctl start vmtoolsd.service
-#sudo systemctl start libvirtd.service
-#sudo systemctl start vboxdrv.service
+sudo systemctl start vmtoolsd.service
+sudo systemctl start libvirtd.service
+sudo systemctl start vboxdrv.service
 
 sudo /etc/init.d/vmware start
 sudo /etc/init.d/vmware-USBArbitrator
 sudo /etc/init.d/vmware-workstation-server start
 
-packer build -parallel=false template.json
-pscp -i ~/Data/Putty/root-virtual.lavabit.com.priv.ppk magma-centos-*-0.*.box root@osheana.virtual.lavabit.com:/var/www/html/downloads/
+packer build -parallel=false magma.json
+packer build -parallel=false magma-centos6.json
+packer build -parallel=false magma-centos7.json
+#pscp -i ~/Data/Putty/root-virtual.lavabit.com.priv.ppk magma-centos-*-0.*.box root@osheana.virtual.lavabit.com:/var/www/html/downloads/
 
