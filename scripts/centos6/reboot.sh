4c28e056658707d940e9ac7bccc713e001787515
Ladar Levison <ladar@lavabit.com>
2016-12-08 01:07:57 -0600

    Tweaked reboot script to try shutting down the network cleanly.

--- scripts/centos6/reboot.sh
+++ scripts/centos6/reboot.sh
@@ -5,12 +5,12 @@ printf "Rebooting onto the newly installed kernel of popcorn. Yummy.\n"
 
 # Schedule a reboot, but give the computer time to cleanly shutdown the 
 # network interface first.
-reboot
+shutdown --reboot --no-wall +1
 
 # If the network interface doesn't properly shutdown, the provisioner may hang, 
 # so we stop the interface manually.  
-# service network stop
+service network stop
 
 # To prevent the next provisioning script from launching during the reboot
 # process a sleep directive is issued blocking the return of this script.
-sleep 60
+sleep 300
