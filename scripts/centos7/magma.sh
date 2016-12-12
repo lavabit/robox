92eea249116275413c8b318e69eb6c6929f7686e
Ladar Levison <ladar@lavabit.com>
2016-12-12 14:44:34 -0600

    Updated the magma build script.

--- scripts/centos7/magma.sh
+++ scripts/centos7/magma.sh
@@ -12,37 +12,49 @@ fi
 cat <<EOF > $OUTPUT
 #!/bin/bash
 
+error() {
+  if [ $? -ne 0 ]; then
+    printf "\n\nmagma daemon compilation failed...\n\n";
+    exit 1
+  fi
+}
+
+if [ -x /usr/bin/id ]; then
+  ID=`/usr/bin/id -u`
+  if [ -n "$ID" -a "$ID" -eq 0 ]; then
+    service mysqld start
+    service havegd start
+    service postfix start
+    service memcached start
+  fi
+fi
+
 # Temporary [hopefully] workaround to avoid [yet another] bug in NSS.
 export NSS_DISABLE_HW_AES=1
 
 # Clone the magma repository off Github.
-git clone https://github.com/lavabit/magma.git magma-develop
+git clone https://github.com/lavabit/magma.git magma-develop; error
+cd magma-develop; error
 
-# Alternatively, grab the latest copy of the development branch using wget.
-# wget --quiet https://github.com/lavabit/magma/archive/develop.tar.gz
-# tar xzvf develop.tar.gz
-
-# Linkup the scripts and clean up the permissions.
-magma-develop/dev/scripts/linkup.sh
+# Clean up the permissions.
 chmod g=,o= magma-develop/sandbox/etc/localhost.localdomain.pem
 chmod g=,o= magma-develop/sandbox/etc/dkim.localhost.localdomain.pem
 
 # Compile the dependencies into a shared library.
-magma-develop/dev/scripts/builders/build.lib.sh all
+dev/scripts/builders/build.lib.sh all; error
 
 # Compile the daemon and then compile the unit tests.
-magma-develop/dev/scripts/builders/build.magma.sh
-magma-develop/dev/scripts/builders/build.check.sh
+make all; error
 
 # Reset the sandbox database and storage files.
-magma-develop/dev/scripts/database/schema.reset.sh
+dev/scripts/database/schema.reset.sh; error
 
 # Enable the anti-virus engine and update the signatures.
-magma-develop/dev/scripts/freshen/freshen.clamav.sh 2>&1 | grep -v WARNING | grep -v PANIC
-sed -i -e "s/virus.available = false/virus.available = true/" magma-develop/sandbox/etc/magma.sandbox.config
+dev/scripts/freshen/freshen.clamav.sh 2>&1 | grep -v WARNING | grep -v PANIC; error
+sed -i -e "s/virus.available = false/virus.available = true/" sandbox/etc/magma.sandbox.config
 
 # Run the unit tests.
-magma-develop/dev/scripts/launch/check.run.sh
+check.run.sh; error
 
 # Alternatively, run the unit tests atop Valgrind. 
 # Note this takes awhile when the anti-virus engine is enabled.
@@ -52,7 +64,7 @@ magma-develop/dev/scripts/launch/check.run.sh
 (magma-develop/dev/scripts/launch/magma.run.sh) &
 
 # Give the daemon time to start before exiting.
-sleep 15
+sleep 60
 
 # Exit wit a zero so Vagrant doesn't think a failed unit test is a provision failure.
 exit 0
