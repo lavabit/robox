0c7c4713bf3c015b9b34b0a90f6506dc6d71d389
Ladar Levison <ladar@lavabit.com>
2016-12-16 01:11:27 -0600

    Updated the docker.sh to generate all three docker images.

--- docker.sh
+++ docker.sh
@@ -10,35 +10,88 @@ BUILDDATE=$(date +%Y%m%d)
 
 cd $BASE
 
-# Build the boxes.
-time packer build -var "box_version=$VERSION" magma-docker.json
+# Validate the templates before building.
+packer validate magma-docker.json
+if [[ $? != 0 ]]; then
+  printf "\a"; sleep 1; printf "\a"; sleep 1; printf "\a"
+  tput setaf 1; tput bold; printf "\n\npacker templates failed to validate...\n\n"; tput sgr0
+  exit 1
+fi
+
+# Build the virtual machines and extract a file system image for docker.
+packer build magma-docker.json
 if [[ $? != 0 ]]; then
   rm -rf packer_cache/
   exit 1
 fi
 
+#
+# magma-docker
+#
 cat << EOF > magma-docker-output/Dockerfile
 FROM scratch
 MAINTAINER Ladar Levison <ladar@lavabit.com>
-ADD magma-centos6-docker-$VERSION.tar.gz /
-
-LABEL name="Magma Build Image" \\
-    vendor="Lavabit" \\
-    license="AGPLv3" \\
-    build-date="$BUILDDATE"
-
+LABEL name="Magma Build Image" vendor="Lavabit" license="AGPLv3" build-date="$BUILDDATE"
+ADD magma-docker-$VERSION.tar.gz /
 ENTRYPOINT ["/root/magma-build.sh"]
 EOF
 
 TEMPDIR=`mktemp -d`
 guestmount -a magma-docker-output/magma-docker -i --ro $TEMPDIR
 cd $TEMPDIR
-tar --numeric-owner --exclude=/tmp/magma-docker.tar -cz . > $BASE/magma-docker-output/magma-centos6-docker-$VERSION.tar.gz
-tar --numeric-owner --exclude=/tmp/magma-docker.tar -cz . > $BASE/magma-centos6-docker-$VERSION.tar.gz
+tar --numeric-owner --exclude=/tmp/magma-docker.tar -cz . > $BASE/magma-docker-output/magma-docker-$VERSION.tar.gz
+tar --numeric-owner --exclude=/tmp/magma-docker.tar -cz . > $BASE/magma-docker-alt-$VERSION.tar.gz
 
 cd $BASE
 guestunmount $TEMPDIR
 rm -f magma-docker-output/magma-docker
 
+docker build --force-rm --tag="magma:$VERSION" magma-docker-output
+
+#
+# magma-centos6-docker
+#
+cat << EOF > magma-centos6-docker-output/Dockerfile
+FROM scratch
+MAINTAINER Ladar Levison <ladar@lavabit.com>
+LABEL name="Magma Build Image" vendor="Lavabit" license="AGPLv3" build-date="$BUILDDATE"
+ADD magma-centos6-docker-$VERSION.tar.gz /
+ENTRYPOINT ["/root/magma-build.sh"]
+EOF
+
+TEMPDIR=`mktemp -d`
+guestmount -a magma-centos6-docker-output/magma-centos6-docker -i --ro $TEMPDIR
+cd $TEMPDIR
+tar --numeric-owner --exclude=/tmp/magma-docker.tar -cz . > $BASE/magma-centos6-docker-output/magma-centos6-docker-$VERSION.tar.gz
+tar --numeric-owner --exclude=/tmp/magma-docker.tar -cz . > $BASE/magma-centos6-docker-alt-$VERSION.tar.gz
+
+cd $BASE
+guestunmount $TEMPDIR
+rm -f magma-docker-output/magma-centos6-docker
+
 docker build --force-rm --tag="magma-centos6:$VERSION" magma-docker-output
-rm -rf packer_cache/ magma-docker-output/
+
+#
+# magma-centos7-docker
+#
+cat << EOF > magma-centos7-docker-output/Dockerfile
+FROM scratch
+MAINTAINER Ladar Levison <ladar@lavabit.com>
+LABEL name="Magma Build Image" vendor="Lavabit" license="AGPLv3" build-date="$BUILDDATE"
+ADD magma-centos7-docker-$VERSION.tar.gz /
+ENTRYPOINT ["/root/magma-build.sh"]
+EOF
+
+TEMPDIR=`mktemp -d`
+guestmount -a magma-centos7-docker-output/magma-centos7-docker -i --ro $TEMPDIR
+cd $TEMPDIR
+tar --numeric-owner --exclude=/tmp/magma-docker.tar -cz . > $BASE/magma-centos7-docker-output/magma-centos7-docker-$VERSION.tar.gz
+tar --numeric-owner --exclude=/tmp/magma-docker.tar -cz . > $BASE/magma-centos7-docker-alt-$VERSION.tar.gz
+
+cd $BASE
+guestunmount $TEMPDIR
+rm -f magma-docker-output/magma-centos7-docker
+
+docker build --force-rm --tag="magma-centos7:$VERSION" magma-docker-output
+
+rm -rf packer_cache/ magma-docker-output/ magma-centos6-docker-output/ magma-centos7-docker-output/
