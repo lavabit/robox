d11869d14e75467db45f4b9f90f8621135ad5afe
Ladar Levison <ladar@lavabit.com>
2016-12-22 00:24:37 -0600

    Made packvnc.sh a loop so it opens consoles for all of the running VMs.

--- downloads/packvnc.sh
+++ downloads/packvnc.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 PORT=`netstat -pnl 2>&1 | grep tcp | grep -E "vmware-vmx|virtualbox|qemu" | awk -F':' '{print $2}' | awk -F' ' '{print $1}' | grep -E "^59"`
-remote-viewer vnc://127.0.0.1:$PORT &> /dev/null &
+
+for p in $PORT; do
+  remote-viewer vnc://127.0.0.1:$p &> /dev/null &
+done
+
 exit 0
