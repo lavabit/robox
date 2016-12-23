10b871e3216ff7f454841775366736f6314afe40
Ladar Levison <ladar@lavabit.com>
2016-12-22 19:07:32 -0600

    Adding config for bare boxes atop VirtualBox and VMWare running the tertiary operating systems.

--- downloads/packvnc.sh
+++ downloads/packvnc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PORT=`netstat -pnl 2>&1 | grep tcp | grep -E "vmware-vmx|virtualbox|qemu" | awk -F':' '{print $2}' | awk -F' ' '{print $1}' | grep -E "^59"`
+PORT=`netstat -pnl 2>&1 | grep tcp | grep -E "vmware-vmx|VBoxHeadless|qemu" | awk -F':' '{print $2}' | awk -F' ' '{print $1}' | grep -E "^59"`
 
 for p in $PORT; do
   remote-viewer vnc://127.0.0.1:$p &> /dev/null &
