2b7cb2470d631b77cbdc335a9c74340f250cc416
Ladar Levison <ladar@lavabit.com>
2017-03-10 02:46:27 -0600

    Trying a different fix for the Debian/Ubuntu tty issue. Added vagrantfile templates for later.

--- scripts/debian8/fixtty.sh
+++ scripts/debian8/fixtty.sh
@@ -2,4 +2,4 @@
 
 # Fix the no tty bug with vagrant.
 # https://github.com/mitchellh/vagrant/issues/1673
-sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
+sed -i -e 's,^.*:/sbin/getty\s\+.*\s\+tty[2-6],#\0,' /etc/inittab
--- scripts/ubuntu1604/fixtty.sh
+++ scripts/ubuntu1604/fixtty.sh
@@ -2,4 +2,8 @@
 
 # Fix the no tty bug with vagrant.
 # https://github.com/mitchellh/vagrant/issues/1673
-sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
+
+sed -i -e 's,^\(ACTIVE_CONSOLES="/dev/tty\).*,\11",' /etc/default/console-setup
+for f in /etc/init/tty[^1]*.conf; do
+  rm --force "$f"
+done
