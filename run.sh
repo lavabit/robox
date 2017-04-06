7a5a503983b1b23cb67b75a1b5b75c12e320ff1a
Ladar Levison <ladar@lavabit.com>
2017-04-06 15:46:59 -0500

    Updated the CentOS 6.9 URLs to use HTTPS and fixed the sha256sum. The VBox addition URL was also updated from 5.0.30 to 5.1.18 so it matches the installed hypervisor.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export VERSION="0.6.4"
+export VERSION="0.6.5"
 export DOCKER_USER="ladar"
 export DOCKER_EMAIL="ladar@lavabitllc.com"
 export DOCKER_PASSWORD="Fs2q5aGWNp6h^^N7qfhH"
@@ -33,10 +33,10 @@ validate() {
 
 # Build the boxes and cleanup the packer cache after each run.
 build() {
-  
+
   export PACKER_LOG="1"
   export PACKER_LOG_PATH="/home/ladar/Desktop/pack-$1.txt"
-  
+
   packer build -on-error=cleanup -parallel=false $1.json
   if [[ $? != 0 ]]; then
     tput setaf 1; tput bold; printf "\n\n$1 images failed to build properly...\n\n"; tput sgr0
