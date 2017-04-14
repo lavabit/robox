8b56fc3cfe8785c3c59a401152730e70661d4379
Ladar Levison <ladar@lavabit.com>
2017-04-14 00:15:56 -0500

    The link check script will now also validate the SHA 256 checksum.

--- links.sh
+++ links.sh
@@ -3,21 +3,52 @@
 function validate {
 
   # Grab just the response header and look for the 200 response code to indicate the link is valid.
-  curl --silent --head  $1 | head -1 | grep --silent --extended-regexp "HTTP/1\.1 200 OK|HTTP/2\.0 200 OK"
+  curl --silent --head "$1" | head -1 | grep --silent --extended-regexp "HTTP/1\.1 200 OK|HTTP/2\.0 200 OK"
 
   # The grep return code tells us whether it found a match in the header or not.
   if [ $? != 0 ]; then
-    printf "Invalid link:\n\t$1\n\n"
+    printf "Link Failure:  $1\n"
+    return 1
   fi
 
+  # Grab the ISO and pipe the data through sha256sum, then compare the checksum value.
+  curl --silent "$1" | sha256sum | grep --silent "$2"
+
+  # The grep return code tells us whether it found a match in the header or not.
+  if [ $? != 0 ]; then
+    SUM=`curl --silent "$1" | sha256sum | awk -F' ' '{print $1}'`
+    printf "Hash Failure:  $1\n"
+    printf "   Expected -  $2\n"
+    printf "      Found -  $SUM\n"
+    return 1
+  fi
+
+  printf "Validated   :  $1\n"
+  return 0
 }
 
+# Count the failures.
+FAILURES=0
+
 # Collect the list of ISO urls.
-ISOURLS=`grep iso_url *.json | awk -F'"' '{print $4}'`
+# ISOURLS=(`grep iso_url magma-docker.json magma-libvirt.json magma-vmware.json magma-virtualbox.json | awk -F'"' '{print $4}'`)
+# ISOSUMS=(`grep "iso_checksum" magma-docker.json magma-libvirt.json magma-vmware.json magma-virtualbox.json | grep -v "iso_checksum_type" | awk -F'"' '{print $4}'`)
+ISOURLS=(`grep iso_url magma-docker.json | awk -F'"' '{print $4}'`)
+ISOSUMS=(`grep "iso_checksum" magma-docker.json | grep -v "iso_checksum_type" | awk -F'"' '{print $4}'`)
 
 # Greet the user.
-printf "\nFound %i links...\n\n" `echo $ISOURLS | wc --words`
+printf "\nFound ${#ISOURLS[@]} links...\n\n"
 
-for l in $ISOURLS; do
-  validate $l
+for ((i = 0; i < ${#ISOURLS[@]}; ++i)); do
+    validate "${ISOURLS[$i]}" "${ISOSUMS[$i]}"
+    if [ $? != 0 ]; then
+      FAILURES=$(( $FAILURES + 1 ))
+    fi
 done
+
+# If any of the links fail, output an extra line to make things cleaner.
+if [ $FAILURES != 0 ]; then
+  printf "\nA total of $FAILURES links failed...\n\n"
+else
+  printf "\n"
+fi
