80ba68cc795d18083c04e760b8d02bb3feae52bf
Ladar Levison <ladar@lavabit.com>
2017-04-27 12:32:33 -0500

    Gave packvnc.sh sudo, forcing the Alpine virtualbox to use sata, and fixed the provider name for the Alpine atlas uploads.

--- res/providers/packvnc.sh
+++ res/providers/packvnc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PORT=`netstat -pnl 2>&1 | grep tcp | grep -E "vmware-vmx|VBoxHeadless|qemu" | awk -F':' '{print $2}' | awk -F' ' '{print $1}' | grep -E "^59"`
+# Sudo is needed to find the vmware-vmx ports.
+PORT=`sudo netstat -pnl 2>&1 | grep tcp | grep -E "vmware-vmx|VBoxHeadless|qemu" | awk -F':' '{print $2}' | awk -F' ' '{print $1}' | grep -E "^59"`
 
 for p in $PORT; do
   remote-viewer vnc://127.0.0.1:$p &> /dev/null &
