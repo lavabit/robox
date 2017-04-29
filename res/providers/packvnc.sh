068a47df89785a4dfd7d2b8e98db160e6daab180
Ladar Levison <ladar@lavabit.com>
2017-04-29 12:27:44 -0500

    Added RDP (aka VirtualBox) support to the packer vnc script.

--- res/providers/packvnc.sh
+++ res/providers/packvnc.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/bin/bash -ux
 
 # Sudo is needed to find the vmware-vmx ports.
-PORT=`sudo netstat -pnl 2>&1 | grep tcp | grep -E "vmware-vmx|VBoxHeadless|qemu" | awk -F':' '{print $2}' | awk -F' ' '{print $1}' | grep -E "^59"`
+PORT=`sudo netstat -pnl 2>&1 | grep tcp | grep -E "vmware-vmx|qemu" | awk -F':' '{print $2}' | awk -F' ' '{print $1}' | grep -E "^59"`
 
 for p in $PORT; do
+  rpm -q virt-viewer &> /dev/null || (printf "\n\nA VNC port was detected, but the Virt Viewer doesn't appear to be available.\n\n" && exit 1)
   remote-viewer vnc://127.0.0.1:$p &> /dev/null &
+done
+
+# Sudo is also needed to find the virtualbox ports.
+PORT=`sudo netstat -pnl 2>&1 | grep tcp | grep "VBoxHeadless" | awk -F':' '{print $2}' | awk -F' ' '{print $1}' | grep -E "^59"`---
+
+for p in $PORT; do
+  rpm -q vinagre &> /dev/null || (printf "\n\nAn RDP port was detected, but Vinagre doesn't appear to be available.\n\n" && exit 1)
+  vinagre --vnc-scale rdp://127.0.0.1:$p &> /dev/null &
 done
 
 exit 0
