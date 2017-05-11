a3f18f8fae6e8bf47471b076b0c2537470a5237f
Ladar Levison <ladar@lavabit.com>
2017-05-11 08:00:25 -0500

    Updated the Gentoo emerge script/kernel config based on the messages provided during the install process.

--- scripts/gentoo/emerge.sh
+++ scripts/gentoo/emerge.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ux
 
 # Force python updates for this system.
-cat <<EOF >> "/etc/portage/package.unmask/python-2.7"
-=dev-lang/python-2.7.5*
-EOF
+# cat <<EOF >> "/etc/portage/package.unmask/python-2.7"
+# =dev-lang/python-2.7.5*
+# EOF
 
 # Update the package database.
 emerge --sync --noconfirm
@@ -12,15 +12,24 @@ emerge --sync --noconfirm
 emerge --update --deep --newuse --with-bdeps=y @world
 
 # Useful tools.
-emerge app-editors/vim net-misc/curl net-misc/wget sys-apps/mlocate app-admin/sysstat app-admin/rsyslog sys-apps/lm_sensors  sys-process/lsof app-admin/sudo
+emerge app-editors/vim net-misc/curl net-misc/wget sys-apps/mlocate app-admin/sysstat app-admin/rsyslog sys-apps/lm_sensors sys-process/lsof app-admin/sudo net-misc/ssh-askpass-fullscreen
 
-# Strart the syslog service.
+# Setup vim as the default editor.
+printf "alias vi=vim\n" >> /etc/profile.d/vim.sh
+
+# Start the syslog service.
 rc-update add rsyslog default && rc-service rsyslog start
 
 # Start the services we just added so the system will track its own performance.
 rc-update add sysstat default && rc-service sysstat start
 
-# Setup vim as the default editor.
-printf "alias vi=vim\n" >> /etc/profile.d/vim.sh
+# This will ensure sensors get initialized during the boot process.
+rc-update add lm_sensors default && rc-service lm_sensors start
+
+# Configure the lm_sensors.conf file.
+/usr/sbin/sensors-detect
+
+# Create an initial mlocate database.
+updatedb
 
 reboot
