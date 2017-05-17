788b02bf1d60aae79b0dd984951cf7ecdfca0d0b
Ladar Levison <ladar@lavabit.com>
2017-05-17 04:29:12 -0500

    Alpine configuration overhauled. Glibc added. APK tool unleashed.

---
+++ scripts/alpine35/glibc.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Download the glibc Alpine packages.
+apk --no-cache add ca-certificates
+wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub
+
+# Unstable.
+wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/unreleased/glibc-2.25-r1.apk
+wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/unreleased/glibc-bin-2.25-r1.apk
+wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/unreleased/glibc-dev-2.25-r1.apk
+wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/unreleased/glibc-i18n-2.25-r1.apk
+
+# Stable but doesn't include the include files.
+# wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.25-r0/glibc-bin-2.25-r0.apk
+# wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.25-r0/glibc-2.25-r0.apk
+# wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.25-r0/glibc-i18n-2.25-r0.apk
+
+# Ensure the compatability library has been removed, to avoid conflicts.
+apk del libc6-compat
+
+# Install glibc.
+apk add glibc-2.25-r1.apk glibc-bin-2.25-r1.apk glibc-dev-2.25-r1.apk glibc-i18n-2.25-r1.apk
+
+#  Generate the English/USA locale.
+/usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8
+
+# Ensure the glibc utilities take precedence for the vagrant user.
+cat <<-EOF > /etc/profile.d/glibc.sh
+ID=\`/usr/bin/id -u\`
+if [ \$ID != 0 ]; then
+  export PATH=/usr/glibc-compat/bin:/usr/glibc-compat/sbin:$PATH
+  export LIBRARY_PATH="/usr/glibc-compat/lib/"
+  export CPATH="/usr/glibc-compat/include/"
+  export C_INCLUDE_PATH="/usr/glibc-compat/include/"
+  export CPLUS_INCLUDE_PATH="/usr/glibc-compat/include/"
+  export OBJC_INCLUDE_PATH="/usr/glibc-compat/include/"
+  export GCC_EXEC_PREFIX="/usr/glibc-compat/"
+fi
+EOF
+
+# VERSION="2.24"
+# PREFIX="/usr/glibc/"
+#
+# # Install the basic build system utilities.
+# apk add --update wget build-base ca-certificates linux-firmware linux-headers \
+# linux-virtgrsec linux-virtgrsec-dev util-linux util-linux-bash-completion \
+# util-linux-dev util-linux-doc syslinux-dev
+#
+# # Grab the tarball with the GNU libc source code.
+# wget -q -O "glibc-${VERSION}.tar.gz" "http://ftp.gnu.org/gnu/glibc/glibc-${VERSION}.tar.gz"
+#
+# # Extract the secrets and get ready to rumble.
+# tar xzvf glibc-${VERSION}.tar.gz
+#
+# # The configure script requrires an independent build directory.
+# mkdir -p glibc-build && cd glibc-build
+#
+# ../glibc-${VERSION}/configure --with-headers=/usr/include/linux/ --prefix=/usr/glibc/
+#
+# # Configure glibc with a prefix so it doesn't conflict with musl.
+# ../glibc-${VERSION}/configure --prefix="${PREFIX}" --libdir="${PREFIX}/lib" \
+# --libexecdir="${PREFIX}/lib" --enable-multi-arch
+#
+# # Compile glibc.
+# make && make install
--- scripts/alpine35/vmware.sh
+++ scripts/alpine35/vmware.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
+#!/bin/bash -eux
 
-error() {
-        if [ $? -ne 0 ]; then
-                printf "\n\nvmware install failed...\n\n";
-                exit 1
-        fi
-}
-
+# Ensure dmidecode is available.
+apk add dmidecode
 
 # Bail if we are not running inside VMWare.
 if [[ `dmidecode -s system-product-name` != "VMware Virtual Platform" ]]; then
@@ -16,25 +11,32 @@ fi
 # Install the VMWare Tools from the Linux ISO.
 printf "Installing the VMWare Tools.\n"
 
-apk add --force open-vm-tools; error
+# Add the community repository.
+printf "@community https://dl-3.alpinelinux.org/alpine/v3.5/community\n@community https://mirror.leaseweb.com/alpine/v3.5/community\n" >> /etc/apk/repositories
+
+# Update the APK cache.
+apk update
+
+# Install the Open VMWare Tools.
+apk add open-vm-tools
 
 # Autostart the open-vm-tools.
-rc-update add open-vm-tools default && rc-service open-vm-tools start; error
-
-#mkdir -p /mnt/vmware; error
-#mount -o loop /root/linux.iso /mnt/vmware; error
-
-#cd /tmp; error
-#tar xzf /mnt/vmware/VMwareTools-*.tar.gz; error
-
-#umount /mnt/vmware; error
-rm -rf /root/linux.iso; error
-
-#/tmp/vmware-tools-distrib/vmware-install.pl -d; error
-#rm -rf /tmp/vmware-tools-distrib; error
+rc-update add open-vm-tools default && rc-service open-vm-tools start
 
 # Boosts the available entropy which allows magma to start faster.
-apk add --force haveged; error
+apk add haveged
 
 # Autostart the haveged daemon.
-rc-update add haveged default && rc-service haveged start; error
+rc-update add haveged default && rc-service haveged start
+
+# Uncomment if you'd prefer to build the guest additions from source.
+# mkdir -p /mnt/vmware
+# mount -o loop /root/linux.iso /mnt/vmware
+# cd /tmp
+# tar xzf /mnt/vmware/VMwareTools-*.tar.gz
+# /tmp/vmware-tools-distrib/vmware-install.pl -d
+# rm -rf /tmp/vmware-tools-distrib
+# umount /mnt/vmware
+
+# When we're done delete the tools ISO.
+rm -rf /root/linux.iso
