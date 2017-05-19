5d822e3865e448b8726f0385ad06ff33576cacfc
Ladar Levison <ladar@lavabit.com>
2017-05-19 17:30:46 -0500

    Finally got the generic OpenSUSE builds to work as intended

--- scripts/opensuse42/zypper.sh
+++ scripts/opensuse42/zypper.sh
@@ -1,16 +1,27 @@
-#!/bin/sh -eux
+#!/bin/bash -eux
 
 version=`grep VERSION= /etc/os-release | cut -f2 -d\" | cut -f1 -d\ `
 
+# Remove locks to avoid dependency problems.
+zypper --non-interactive removelock "*"
 
-# Remove locks to avoid dependency problems
-zypper --non-interactive rl \*
+# Remove the default installation repository.
+zypper --non-interactive removerepo "openSUSE-Leap-${version}-0"
 
-zypper removerepo "openSUSE-${version}-0"
+# Add the default repositories for this release.
+zypper --non-interactive addrepo https://download.opensuse.org/distribution/leap/${version}/repo/oss/ openSUSE-Leap-${version}-Oss
+zypper --non-interactive addrepo https://download.opensuse.org/distribution/leap/${version}/repo/non-oss/ openSUSE-Leap-${version}-Non-Oss
+zypper --non-interactive addrepo https://download.opensuse.org/update/leap/${version}/oss/ openSUSE-Leap-${version}-Update
+zypper --non-interactive addrepo https://download.opensuse.org/update/leap/${version}/non-oss/ openSUSE-Leap-${version}-Update-Non-Oss
 
-zypper ar https://mirrors.kernel.org/opensuse/distribution/leap/${version}/repo/oss/ openSUSE-Leap-${version}-Oss
-zypper ar https://mirrors.kernel.org/opensuse/distribution/leap/${version}/repo/non-oss/ openSUSE-Leap-${version}-Non-Oss
-zypper ar https://mirrors.kernel.org/opensuse/update/leap/${version}/oss/ openSUSE-Leap-${version}-Update
-zypper ar https://mirrors.kernel.org/opensuse/update/leap/${version}/non-oss/ openSUSE-Leap-${version}-Update-Non-Oss
+# Clean out any stale cache data.
+zypper --non-interactive clean --all
 
-zypper refresh
+# Refresh the repository metadata.
+zypper --non-interactive refresh
+# echo "Zypper repositories refreshed."
+
+# Update the system packages.
+zypper --non-interactive update --auto-agree-with-licenses
+# echo "System updates have been applied."
+# exit 0
