4d29d1d981c7e615afeacc2200b3b5281463ce5b
Ladar Levison <ladar@lavabit.com>
2017-05-19 17:29:58 -0500

    Added the boxes script.

--- res/providers/packvnc.sh
+++ res/providers/packvnc.sh
@@ -6,8 +6,8 @@ PORT=`sudo netstat -pnl 2>&1 | grep tcp | grep -E "vmware-vmx|qemu|qemu-kvm" | a
 for p in $PORT; do
   #rpm -q virt-viewer &> /dev/null || (printf "\n\nA VNC port was detected, but the Virt Viewer doesn't appear to be available.\n\n" && exit 1)
   #remote-viewer vnc://127.0.0.1:$p &> /dev/null &
-  rpm -q vinagre &> /dev/null || (printf "\n\nA VNC port was detected, but Vinagre doesn't appear to be available.\n\n" && exit 1)
-  vinagre vnc://127.0.0.1:$p &> /dev/null &
+  rpm -q vinagre  &> /dev/null || (printf "\n\nA VNC port was detected, but Vinagre doesn't appear to be available.\n\n" && exit 1)
+  vinagre --vnc-scale vnc://127.0.0.1:$p &> /dev/null &
 done
 
 # Sudo is also needed to find the virtualbox ports.
---
+++ res/scripts/boxes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo ""
+curl --silent https://atlas.hashicorp.com/$1/boxes/$2 | jq ".versions[0].version"
+echo ""
+# curl --silent https://atlas.hashicorp.com/$1/boxes/$2 | jq -M ".versions[0].providers[].name"
+curl --silent https://atlas.hashicorp.com/$1/boxes/$2 | jq -M "if .versions[0] != null then .versions[0].providers[].name else empty end"
+
+echo ""
