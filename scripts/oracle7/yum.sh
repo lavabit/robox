2539a2acc2cbf2b6473b9908de65cac413820c25
Ladar Levison <ladar@lavabit.com>
2017-05-25 22:27:23 -0500

    Added a VirtualBox config for a generic Oracle 7 image.

---
+++ scripts/oracle7/yum.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eux
+
+# Update the base install first.
+yum --assumeyes update
+
+# The basic utilities we'd expect to find.
+yum --assumeyes install man git sudo lsof wget curl telnet mlocate sysstat dmidecode bind-utils yum-utils vim-enhanced
+
+# Remove the stray rpm save file.
+mv --force /etc/nsswitch.conf.rpmnew /etc/nsswitch.conf
+
+# Update the locate database.
+/etc/cron.daily/mlocate.cron
+
+# Schedule a reboot, but give the computer time to cleanly shutdown the
+# network interface first.
+shutdown --reboot --no-wall +1
