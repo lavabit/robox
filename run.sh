1ad62f80b9425e583bb93244df42687f13040a67
Ladar Levison <ladar@lavabit.com>
2017-06-14 03:24:52 -0600

    Added hyperv awareness to the run.sh script.

--- run.sh
+++ run.sh
@@ -8,16 +8,21 @@
 # Limit the number of cpus packer will use.
 # export GOMAXPROCS="2"
 
+# Ensure a consistent working directory so relative paths work.
+LINK=`readlink -f $0`
+BASE=`dirname $LINK`
+cd $BASE
+
 # Credentials and tokens.
 export VERSION="0.9.8"
 source .credentialsrc
 
 # The list of packer config files.
-FILES="magma-docker.json magma-libvirt.json magma-vmware.json magma-virtualbox.json generic-libvirt.json generic-vmware.json generic-virtualbox.json"
+FILES="magma-docker.json magma-hyperv.json magma-vmware.json magma-libvirt.json magma-virtualbox.json generic-hyperv.json generic-vmware.json generic-libvirt.json generic-virtualbox.json"
 
 # Collect the list of ISO urls.
 ISOURLS=(`grep -E "iso_url|guest_additions_url" $FILES | awk -F'"' '{print $4}'`)
-ISOSUMS=(`grep -E "iso_checksum|guest_additions_sha256" magma-docker.json magma-libvirt.json magma-vmware.json magma-virtualbox.json generic-libvirt.json generic-vmware.json generic-virtualbox.json | grep -v "iso_checksum_type" | awk -F'"' '{print $4}'`)
+ISOSUMS=(`grep -E "iso_checksum|guest_additions_sha256" $FILES | grep -v "iso_checksum_type" | awk -F'"' '{print $4}'`)
 
 # Collect the list of box names.
 MAGMA_BOXES=`grep -E '"name":' $FILES | awk -F'"' '{print $4}' | grep "magma-" | sort --field-separator=- -k 3i -k 2.1,2.0`
@@ -26,15 +31,13 @@ LINEAGE_BOXES=`grep -E '"name":' $FILES | awk -F'"' '{print $4}' | grep "lineage
 BOXES="$LINEAGE_BOXES $GENERIC_BOXES $MAGMA_BOXES"
 
 # Collect the list of box tags.
-MAGMA_TAGS=`grep -E '"box_tag":' $FILES | awk -F'"' '{print $4}' | grep "magma" | sort -u --field-separator=- -k 3i -k 2.1,2.0`
-GENERIC_TAGS=`grep -E '"box_tag":' $FILES | awk -F'"' '{print $4}' | grep "generic" | sort -u --field-separator=- -k 2i -k 1.1,1.0`
-LINEAGE_TAGS=`grep -E '"box_tag":' $FILES | awk -F'"' '{print $4}' | grep "lineage" | sort -u --field-separator=- -k 2i -k 1.1,1.0`
+# MAGMA_TAGS=`grep -E '"box_tag":' $FILES | awk -F'"' '{print $4}' | grep "magma" | sort -u --field-separator=- -k 3i -k 2.1,2.0`
+# GENERIC_TAGS=`grep -E '"box_tag":' $FILES | awk -F'"' '{print $4}' | grep "generic" | sort -u --field-separator=- -k 2i -k 1.1,1.0`
+# LINEAGE_TAGS=`grep -E '"box_tag":' $FILES | awk -F'"' '{print $4}' | grep "lineage" | sort -u --field-separator=- -k 2i -k 1.1,1.0`
+MAGMA_TAGS=`grep -E '"artifact":' $FILES | awk -F'"' '{print $4}' | grep "magma" | sort -u --field-separator=- -k 3i -k 2.1,2.0`
+GENERIC_TAGS=`grep -E '"artifact":' $FILES | awk -F'"' '{print $4}' | grep "generic" | sort -u --field-separator=- -k 2i -k 1.1,1.0`
+LINEAGE_TAGS=`grep -E '"artifact":' $FILES | awk -F'"' '{print $4}' | grep "lineage" | sort -u --field-separator=- -k 2i -k 1.1,1.0`
 TAGS="$LINEAGE_TAGS $GENERIC_TAGS $MAGMA_TAGS"
-
-# Ensure a consistent working directory so relative paths work.
-LINK=`readlink -f $0`
-BASE=`dirname $LINK`
-cd $BASE
 
 function start() {
   # Disable IPv6 or the VMware builder won't be able to load the Kick Start configuration.
@@ -99,12 +102,23 @@ function verify_json() {
   fi
 }
 
+# Make sure the logging directory is avcailable. If it isn't, then create it.
+function verify_logdir {
+
+  if [ ! -d output ]; then
+    mkdir -p output/logs
+  elif [ ! -d output/logs ]; then
+    mkdir output/logs
+  fi
+}
+
 # Build the boxes and cleanup the packer cache after each run.
 function build() {
 
+  verify_logdir
   export PACKER_LOG="1"
   export TIMESTAMP=`date +"%s"`
-  export PACKER_LOG_PATH="/home/ladar/Desktop/packer-logs/$1-${TIMESTAMP}.txt"
+  export PACKER_LOG_PATH="$BASE/output/logs/$1-${TIMESTAMP}.txt"
 
   packer build -on-error=cleanup -parallel=false $1.json
 
@@ -117,28 +131,29 @@ function build() {
 # Build an individual box.
 function box() {
 
+  verify_logdir
   export PACKER_LOG="1"
   export TIMESTAMP=`date +"%s"`
 
-  export PACKER_LOG_PATH="/home/ladar/Desktop/packer-logs/magma-docker-${TIMESTAMP}.txt"
+  export PACKER_LOG_PATH="$BASE/output/logs/magma-docker-${TIMESTAMP}.txt"
   packer build -on-error=cleanup -parallel=false -only=$1 magma-docker.json
 
-  export PACKER_LOG_PATH="/home/ladar/Desktop/packer-logs/magma-vmware-${TIMESTAMP}.txt"
+  export PACKER_LOG_PATH="$BASE/output/logs/magma-vmware-${TIMESTAMP}.txt"
   packer build -on-error=cleanup -parallel=false -only=$1 magma-vmware.json
 
-  export PACKER_LOG_PATH="/home/ladar/Desktop/packer-logs/magma-libvirt-${TIMESTAMP}.txt"
+  export PACKER_LOG_PATH="$BASE/output/logs/magma-libvirt-${TIMESTAMP}.txt"
   packer build -on-error=cleanup -parallel=false -only=$1 magma-libvirt.json
 
-  export PACKER_LOG_PATH="/home/ladar/Desktop/packer-logs/magma-virtualbox-${TIMESTAMP}.txt"
+  export PACKER_LOG_PATH="$BASE/output/logs/magma-virtualbox-${TIMESTAMP}.txt"
   packer build -on-error=cleanup -parallel=false -only=$1 magma-virtualbox.json
 
-  export PACKER_LOG_PATH="/home/ladar/Desktop/packer-logs/generic-vmware-${TIMESTAMP}.txt"
+  export PACKER_LOG_PATH="$BASE/output/logs/generic-vmware-${TIMESTAMP}.txt"
   packer build -on-error=cleanup -parallel=false -only=$1 generic-vmware.json
 
-  export PACKER_LOG_PATH="/home/ladar/Desktop/packer-logs/generic-libvirt-${TIMESTAMP}.txt"
+  export PACKER_LOG_PATH="$BASE/output/logs/generic-libvirt-${TIMESTAMP}.txt"
   packer build -on-error=cleanup -parallel=false -only=$1 generic-libvirt.json
 
-  export PACKER_LOG_PATH="/home/ladar/Desktop/packer-logs/generic-virtualbox-${TIMESTAMP}.txt"
+  export PACKER_LOG_PATH="$BASE/output/logs/generic-virtualbox-${TIMESTAMP}.txt"
   packer build -on-error=cleanup -parallel=false -only=$1 generic-virtualbox.json
 
   if [[ $? != 0 ]]; then
@@ -169,9 +184,11 @@ function sums() {
 
 function validate() {
   verify_json magma-docker
+  verify_json magma-hyperv
   verify_json magma-vmware
   verify_json magma-libvirt
   verify_json magma-virtualbox
+  verify_json generic-hyperv
   verify_json generic-vmware
   verify_json generic-libvirt
   verify_json generic-virtualbox
@@ -205,11 +222,10 @@ function available() {
     LIST=($TAGS)
 
     for ((i = 0; i < ${#LIST[@]}; ++i)); do
-
       ORGANIZATION=`echo ${LIST[$i]} | awk -F'/' '{print $1}'`
       BOX=`echo ${LIST[$i]} | awk -F'/' '{print $2}'`
 
-      PROVIDER="vmware"
+      PROVIDER="hyperv"
       curl --silent --head "https://vagrantcloud.com/api/v1/box/${ORGANIZATION}/${BOX}/version/${VERSION}/provider/${PROVIDER}?access_token=${ATLAS_TOKEN}" | head -1 | grep --silent --extended-regexp "HTTP/1\.1 200 OK|HTTP/2\.0 200 OK"
 
       if [ $? != 0 ]; then
@@ -230,6 +246,16 @@ function available() {
       fi
 
       PROVIDER="virtualbox"
+      curl --silent --head "https://vagrantcloud.com/api/v1/box/${ORGANIZATION}/${BOX}/version/${VERSION}/provider/${PROVIDER}?access_token=${ATLAS_TOKEN}" | head -1 | grep --silent --extended-regexp "HTTP/1\.1 200 OK|HTTP/2\.0 200 OK"
+
+      if [ $? != 0 ]; then
+        let MISSING+=1
+        printf "Box  -  "; tput setaf 1; printf "${LIST[$i]} ${PROVIDER}\n"; tput sgr0
+      else
+        printf "Box  +  "; tput setaf 2; printf "${LIST[$i]} ${PROVIDER}\n"; tput sgr0
+      fi
+
+      PROVIDER="vmware_desktop"
       curl --silent --head "https://vagrantcloud.com/api/v1/box/${ORGANIZATION}/${BOX}/version/${VERSION}/provider/${PROVIDER}?access_token=${ATLAS_TOKEN}" | head -1 | grep --silent --extended-regexp "HTTP/1\.1 200 OK|HTTP/2\.0 200 OK"
 
       if [ $? != 0 ]; then
@@ -264,16 +290,24 @@ function login() {
 }
 
 function magma() {
-  login ; build magma-docker
-  build magma-vmware
-  build magma-libvirt
-  build magma-virtualbox
+  if [[ $OS == "Windows_NT" ]]; then
+    build magma-hyperv
+  else
+    login ; build magma-docker
+    build magma-vmware
+    build magma-libvirt
+    build magma-virtualbox
+  fi
 }
 
 function generic() {
-  build generic-vmware
-  build generic-libvirt
-  build generic-virtualbox
+  if [[ $OS == "Windows_NT" ]]; then
+    build generic-hyperv
+  else
+    build generic-vmware
+    build generic-libvirt
+    build generic-virtualbox
+  fi
 }
 
 function all() {
@@ -305,9 +339,12 @@ elif [[ $1 == "generic" ]]; then generic
 elif [[ $1 == "magma-docker" || $1 == "magma-docker.json" ]]; then login ; build magma-docker
 elif [[ $1 == "magma-vmware" || $1 == "magma-vmware.json" ]]; then build magma-vmware
 elif [[ $1 == "magma-libvirt" || $1 == "magma-libvirt.json" ]]; then build magma-libvirt
+elif [[ $1 == "magma-hyperv" || $1 == "magma-hyperv.json" ]]; then build magma-hyperv
 elif [[ $1 == "magma-virtualbox" || $1 == "magma-virtualbox.json" ]]; then build magma-virtualbox
+
 elif [[ $1 == "generic-vmware" || $1 == "generic-vmware.json" ]]; then build generic-vmware
 elif [[ $1 == "generic-libvirt" || $1 == "generic-libvirt.json" ]]; then build generic-libvirt
+elif [[ $1 == "generic-hyperv" || $1 == "generic-hyperv.json" ]]; then build generic-hyperv
 elif [[ $1 == "generic-virtualbox" || $1 == "generic-virtualbox.json" ]]; then build generic-virtualbox
 
 # Build a specific box.
