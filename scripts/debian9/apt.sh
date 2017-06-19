e23abe87ab021977abe74005e95470d806c97fe9
Ladar Levison <ladar@lavabit.com>
2017-06-19 16:10:30 -0500

    Updated links. Added Generic Debian 9 config.

---
+++ scripts/debian9/apt.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+error() {
+        if [ $? -ne 0 ]; then
+                printf "\n\napt failed...\n\n";
+                exit 1
+        fi
+}
+
+# To allow for autmated installs, we disable interactive configuration steps.
+export DEBIAN_FRONTEND=noninteractive
+
+# Ensure the server includes any necessary updates.
+apt-get --assume-yes update; error
+apt-get --assume-yes dist-upgrade; error
+
+# The packages needed to compile magma.
+apt-get --assume-yes install gcc g++ gcc-multilib make autoconf automake libtool flex bison gdb valgrind valgrind-dbg libpython2.7 libc6-dev libc++-dev libncurses5-dev libmpfr4 libmpfr-dev patch make cmake libarchive13 libbsd-dev libsubunit-dev libsubunit0 pkg-config; error
+
+# The memcached server.
+apt-get --assume-yes install memcached libevent-dev; error
+
+# The postfix server for message relays.
+apt-get --assume-yes install postfix postfix-cdb libcdb1 ssl-cert; error
+
+# Need to retrieve the source code.
+apt-get --assume-yes install git git-man liberror-perl rsync wget; error
+
+# Needed to run the watcher and status scripts.
+apt-get --assume-yes install sysstat inotify-tools; error
+
+# Needed to run the stacie script.
+apt-get --assume-yes install python-crypto python-cryptography; error
+
+# Boosts the available entropy which allows magma to start faster.
+apt-get --assume-yes install haveged; error
+
+# Reboot onto the new kernel (if applicable).
+$(shutdown -r +1) &
---
+++ scripts/debian9/virtualbox.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+error() {
+        if [ $? -ne 0 ]; then
+                printf "\n\nvbox install failed...\n\n";
+                exit 1
+        fi
+}
+
+# Bail if we are not running inside VirtualBox.
+if [[ `dmidecode -s system-product-name` != "VirtualBox" ]]; then
+    exit 0
+fi
+
+# Install the Virtual Box Tools from the Linux Guest Additions ISO.
+printf "Installing the Virtual Box Tools.\n"
+
+# Read in the version number.
+VBOXVERSION=`cat /root/VBoxVersion.txt`
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get --assume-yes install dkms build-essential module-assistant linux-headers-$(uname -r); error
+
+mkdir -p /mnt/virtualbox; error
+mount -o loop /root/VBoxGuestAdditions.iso /mnt/virtualbox; error
+
+sh /mnt/virtualbox/VBoxLinuxAdditions.run; error
+ln -s /opt/VBoxGuestAdditions-$VBOXVERSION/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions; error
+
+umount /mnt/virtualbox; error
+rm -rf /root/VBoxVersion.txt; error
+rm -rf /root/VBoxGuestAdditions.iso; error
+
+# Boosts the available entropy which allows magma to start faster.
+apt-get --assume-yes install haveged; error
+
+# Autostart the haveged daemon.
+systemctl enable haveged.service
