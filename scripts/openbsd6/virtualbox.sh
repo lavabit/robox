e03f93827dd8199ff7c415257df34a4849002d72
Ladar Levison <ladar@lavabit.com>
2017-06-23 09:29:18 -0500

    Updated the OpenBSD config so it will properly setup the vagrant user account and package install directories.

---
+++ scripts/openbsd6/virtualbox.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eux
+
+# Ensure dmideocode is available.
+pkg_add -I dmidecode
+
+# Bail if we are not running inside VirtualBox.
+if [[ `dmidecode -s system-product-name` != "VirtualBox" ]]; then
+    exit 0
+fi
