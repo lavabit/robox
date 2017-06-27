7230a41469720d2597442ab81abde7625511ed94
Ladar Levison <ladar@lavabit.com>
2017-06-26 22:59:03 -0500

    Updated the Debian 8 apt script. Removed the full-upgrade operation.

--- scripts/debian8/apt.sh
+++ scripts/debian8/apt.sh
@@ -14,8 +14,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get --assume-yes update; error
 apt-get --assume-yes upgrade; error
 apt-get --assume-yes dist-upgrade; error
-apt-get --assume-yes full-upgrade; error
-
 
 # The packages needed to compile magma.
 apt-get --assume-yes install gcc g++ gcc-multilib make autoconf automake libtool flex bison gdb valgrind valgrind-dbg libpython2.7 libc6-dev libc++-dev libncurses5-dev libmpfr4 libmpfr-dev patch make cmake libarchive13 libbsd-dev libsubunit-dev libsubunit0 pkg-config; error
