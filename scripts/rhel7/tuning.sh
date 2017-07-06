e9bb01554aa2364e4fc62bd443c702ce58ae3871
Ladar Levison <ladar@lavabit.com>
2017-07-06 17:21:45 -0500

    Adding the RHEL 7 script/kickstart variants.

---
+++ scripts/rhel7/tuning.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Configure tuned
+yum --quiet --assumeyes install tuned
+systemctl enable tuned
+systemctl start tuned
+
+# Set the profile to virtual guest.
+tuned-adm profile virtual-guest
+
+# Configure grub to wait just 1 second before booting
+sed -i -e 's/^GRUB_TIMEOUT=[0-9]\+$/GRUB_TIMEOUT=1/' /etc/default/grub
+grub2-mkconfig -o /boot/grub2/grub.cfg
