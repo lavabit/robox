3aa66cfbfa04360b8733886c4ae2cd0aef5d8f2d
Ladar Levison <ladar@lavabit.com>
2017-07-13 00:51:47 -0500

    Initial generic Fedora 26 configs added.

---
+++ scripts/fedora26/virtualbox.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+error() {
+        if [ $? -ne 0 ]; then
+                printf "\n\nvbox install failed...\n\n";
+                exit 1
+        fi
+}
+
+# Bail if we are not running inside VirtualBox.
+if [[ `dmidecode -s system-product-name` != "VirtualBox" ]]; then
+    exit 0
+fi
+
+# Install the Virtual Box Tools from the Linux Guest Additions ISO.
+printf "Installing the Virtual Box Tools.\n"
+
+# Read in the version number.
+VBOXVERSION=`cat /root/VBoxVersion.txt`
+
+dnf --assumeyes install bzip2
+
+mkdir -p /mnt/virtualbox; error
+mount -o loop /root/VBoxGuestAdditions.iso /mnt/virtualbox; error
+
+sh /mnt/virtualbox/VBoxLinuxAdditions.run --nox11; error
+ln -s /opt/VBoxGuestAdditions-$VBOXVERSION/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions; error
+
+umount /mnt/virtualbox; error
+rm -rf /root/VBoxVersion.txt; error
+rm -rf /root/VBoxGuestAdditions.iso; error
