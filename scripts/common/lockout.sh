668eaa93ca205958332ffbc1db2d1af9501a6ea9
Ladar Levison <ladar@lavabit.com>
2017-08-29 21:19:15 -0500

    Make the zero disk utility a little more reliable.

--- scripts/common/lockout.sh
+++ scripts/common/lockout.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -eux
 
 # Randomize the root password and then lock the root account.
-LOCKPWD=`dd if=/dev/urandom count=50 | md5sum | awk -F' ' '{print $1}'`
+LOCKPWD=`dd if=/dev/urandom count=128 | md5sum | awk -F' ' '{print $1}'`
 printf "$LOCKPWD\n$LOCKPWD\n" | passwd root
-
 
 # Handle builds using the busybox version of df/dd/rm which use different command line arguments.
 if [[ "$PACKER_BUILD_NAME" =~ ^magma-alpine3[5-6]-vmware$|^magma-alpine3[5-6]-hyperv$|^magma-alpine3[5-6]-libvirt$|^magma-alpine3[5-6]-virtualbox$|^generic-alpine3[5-6]-vmware$|^generic-alpine3[5-6]-hyperv$|^generic-alpine3[5-6]-libvirt$|^generic-alpine3[5-6]-virtualbox$ ]]; then
