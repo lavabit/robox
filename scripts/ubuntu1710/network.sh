ee746ee66f4a3139c998178f0ec7ae40768664f3
Ladar Levison <ladar@lavabit.com>
2017-10-24 18:11:21 -0500

    Fixed the Ubuntu 17.10 networking script.

--- scripts/ubuntu1710/network.sh
+++ scripts/ubuntu1710/network.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eux
+#!/bin/bash
 
 # Disable IPv6 for the current boot.
 sysctl net.ipv6.conf.all.disable_ipv6=1
@@ -10,9 +10,9 @@ printf "\nnet.ipv6.conf.all.disable_ipv6 = 1\n" >> /etc/sysctl.conf
 printf "\nnameserver 4.2.2.1\n" > /etc/resolv.conf
 
 # Set the hostname, and then ensure it will resolve properly.
-if [[ "$PACKER_BUILD_NAME" =~ ^generic-ubuntu1704-vmware$|^generic-ubuntu1704-libvirt$|^generic-ubuntu1704-virtualbox$ ]]; then
-  printf "ubuntu1704.localdomain\n" > /etc/hostname
-  printf "\n127.0.0.1 ubuntu1704.localdomain\n\n" >> /etc/hosts
+if [[ "$PACKER_BUILD_NAME" =~ ^generic-ubuntu1710-vmware$|^generic-ubuntu1710-libvirt$|^generic-ubuntu1710-virtualbox$ ]]; then
+  printf "ubuntu1710.localdomain\n" > /etc/hostname
+  printf "\n127.0.0.1 ubuntu1710.localdomain\n\n" >> /etc/hosts
 else
   printf "magma.builder\n" > /etc/hostname
   printf "\n127.0.0.1 magma.builder\n\n" >> /etc/hosts
@@ -22,18 +22,17 @@ fi
 sed -i -e 's/^GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"$/GRUB_CMDLINE_LINUX_DEFAULT="\1 net.ifnames=0"/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Clear out the existing automatic ifup rules.
-sed -i -e '/^auto/d' /etc/network/interfaces
-sed -i -e '/^iface/d' /etc/network/interfaces
+cat <<-EOF > /etc/netplan/01-netcfg.yaml
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth0:
+      dhcp4: true
+EOF
 
-# Ensure the loopback, and default network interface are automatically enabled and then dhcp'ed.
-printf "auto lo\n" >> /etc/network/interfaces;
-printf "iface lo inet loopback\n" >> /etc/network/interfaces;
-printf "auto eth0\n" >> /etc/network/interfaces;
-printf "iface eth0 inet dhcp\n" >> /etc/network/interfaces;
-
-# Adding a delay so dhclient will work properly.
-printf "pre-up sleep 2\n" >> /etc/network/interfaces;
+# Apply the network plan configuration.
+netplan generate
 
 # Ensure the networking interfaces get configured on boot.
-systemctl enable networking.service
+systemctl enable systemd-networkd.service
