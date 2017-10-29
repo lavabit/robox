661f8a8d171b8ca94677477296bdebb998035e1e
Ladar Levison <ladar@lavabit.com>
2017-10-29 10:47:43 -0500

    Updated the BSD hostname scripts.

--- scripts/freebsd11/hostname.sh
+++ scripts/freebsd11/hostname.sh
@@ -2,8 +2,11 @@
 
 if [[ "$PACKER_BUILD_NAME" =~ ^magma-freebsd ]]; then
   hostname magma.localdomain
+	sed -i -e "s/hostname=\".*\"/hostname=\"magma.localdomain\"/g" /etc/defaults/rc.conf
 elif [[ "$PACKER_BUILD_NAME" =~ ^generic-freebsd ]]; then
 	hostname freebsd.localdomain
+	sed -i -e "s/hostname=\".*\"/hostname=\"freebsd.localdomain\"/g" /etc/defaults/rc.conf
 else
   hostname bazinga.localdomain
+	sed -i -e "s/hostname=\".*\"/hostname=\"bazinga.localdomain\"/g" /etc/defaults/rc.conf
 fi
--- scripts/openbsd6/hostname.sh
+++ scripts/openbsd6/hostname.sh
@@ -2,8 +2,11 @@
 
 if [[ "$PACKER_BUILD_NAME" =~ ^magma-openbsd ]]; then
   hostname magma.localdomain
+	sed -i -e "s/hostname=\".*\"/hostname=\"magma.localdomain\"/g" /etc/defaults/rc.conf
 elif [[ "$PACKER_BUILD_NAME" =~ ^generic-openbsd ]]; then
 	hostname openbsd.localdomain
+	sed -i -e "s/hostname=\".*\"/hostname=\"openbsd.localdomain\"/g" /etc/defaults/rc.conf
 else
   hostname bazinga.localdomain
+	sed -i -e "s/hostname=\".*\"/hostname=\"bazinga.localdomain\"/g" /etc/defaults/rc.conf
 fi
