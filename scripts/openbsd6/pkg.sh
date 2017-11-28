96682f9b18dcdd4bdeed7ac6226d0117d01859f7
Ladar Levison <ladar@lavabit.com>
2017-11-28 11:53:05 -0600

    Rewrote the common zerodisk/lockout scripts so they seemlessly support Alpine and BSD, alongside the more typical Linux distro.

--- scripts/openbsd6/pkg.sh
+++ scripts/openbsd6/pkg.sh
@@ -3,5 +3,8 @@
 echo "https://ftp.usa.openbsd.org/pub/OpenBSD/" > /etc/installurl
 pkg_add -I curl wget bash sudo-- vim--no_x11
 
+# Since most scripts expect bash to be in the bin directory, create a symlink.
+ln -s /usr/local/bin/bash /bin/bash
+
 echo "kern.allowkmem=1" > /etc/sysctl.conf
 reboot
