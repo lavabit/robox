9d111a1e403f98fdf40f38b68198473509198a58
Ladar Levison <ladar@lavabit.com>
2018-01-16 20:30:47 -0600

    Removed an errant chcon in the Fedora 27 base script.

--- scripts/fedora27/base.sh
+++ scripts/fedora27/base.sh
@@ -39,9 +39,6 @@ printf "export HISTSIZE=\"100000\"\n" > /etc/profile.d/histsize.sh
 chcon "system_u:object_r:bin_t:s0" /etc/profile.d/histsize.sh
 chmod 644 /etc/profile.d/histsize.sh
 
-# Fix the SELinux context.
-chcon system_u:object_r:etc_t:s0 /etc/security/limits.d/50-magmad.conf
-
 # Set the timezone to Pacific time.
 printf "ZONE=\"America/Los_Angeles\"\n" > /etc/sysconfig/clock
 
