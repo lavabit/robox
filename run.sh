530817c50c8b243dfec61d63490960c289e948dd
Ladar Levison <ladar@lavabit.com>
2018-03-19 19:24:07 -0500

    Updated ISO URLs.

--- run.sh
+++ run.sh
@@ -116,7 +116,7 @@ function cache {
 function verify_url {
 
   # Grab just the response header and look for the 200 response code to indicate the link is valid.
-  curl --silent --head "$1" | head -1 | grep --silent --extended-regexp "HTTP/1\.1 200 OK|HTTP/2\.0 200 OK"
+  curl --silent --location --head "$1" | head -1 | grep --silent --extended-regexp "HTTP/1\.1 200 OK|HTTP/2\.0 200 OK"
 
   # The grep return code tells us whether it found a match in the header or not.
   if [ $? != 0 ]; then
