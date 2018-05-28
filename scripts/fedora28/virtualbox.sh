0587459397212ddec4663eec3fc8fd55c13a8f01
Ladar Levison <ladar@lavabit.com>
2018-05-28 04:50:18 -0500

    Use the Fedora 28 guest additions package instead compiling.

--- scripts/fedora28/virtualbox.sh
+++ scripts/fedora28/virtualbox.sh
@@ -4,20 +4,19 @@ error() {
   if [ $? -ne 0 ]; then
     printf "\n\nThe VirtualBox install failed...\n\n"
 
-    if [ -f /var/log/VBoxGuestAdditions.log ]; then
-      printf "\n\n/var/log/VBoxGuestAdditions.log\n\n"
-      cat /var/log/VBoxGuestAdditions.log
-    else
-      printf "\n\nThe /var/log/VBoxGuestAdditions.log is missing...\n\n"
-    fi
-
-    if [ -f /var/log/vboxadd-install.log ]; then
-      printf "\n\n/var/log/vboxadd-install.log\n\n"
-      cat /var/log/vboxadd-install.log
-    else
-      printf "\n\nThe /var/log/vboxadd-install.log is missing...\n\n"
-    fi
-
+    # if [ -f /var/log/VBoxGuestAdditions.log ]; then
+    #   printf "\n\n/var/log/VBoxGuestAdditions.log\n\n"
+    #   cat /var/log/VBoxGuestAdditions.log
+    # else
+    #   printf "\n\nThe /var/log/VBoxGuestAdditions.log is missing...\n\n"
+    # fi
+    #
+    # if [ -f /var/log/vboxadd-install.log ]; then
+    #   printf "\n\n/var/log/vboxadd-install.log\n\n"
+    #   cat /var/log/vboxadd-install.log
+    # else
+    #   printf "\n\nThe /var/log/vboxadd-install.log is missing...\n\n"
+    # fi
     exit 1
   fi
 }
@@ -33,14 +32,17 @@ printf "Installing the Virtual Box Tools.\n"
 # Read in the version number.
 VBOXVERSION=`cat /root/VBoxVersion.txt`
 
-dnf --assumeyes install dkms binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel bzip2
+dnf --assumeyes install virtualbox-guest-additions; error
 
-mkdir -p /mnt/virtualbox; error
-mount -o loop /root/VBoxGuestAdditions.iso /mnt/virtualbox; error
+# dnf --assumeyes install dkms binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel bzip2 kernel-headers kernel-devel kernel-cross-headers
+#
+# mkdir -p /mnt/virtualbox; error
+# mount -o loop /root/VBoxGuestAdditions.iso /mnt/virtualbox; error
+#
+# sh /mnt/virtualbox/VBoxLinuxAdditions.run --nox11; error
+# ln -s /opt/VBoxGuestAdditions-$VBOXVERSION/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions; error
+#
+# umount /mnt/virtualbox; error
 
-sh /mnt/virtualbox/VBoxLinuxAdditions.run --nox11; error
-ln -s /opt/VBoxGuestAdditions-$VBOXVERSION/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions; error
-
-umount /mnt/virtualbox; error
 rm -rf /root/VBoxVersion.txt; error
 rm -rf /root/VBoxGuestAdditions.iso; error
