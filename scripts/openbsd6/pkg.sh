71f090117ccdf2b66c4f4c42c8d5b64a27bbd6c0
Ladar Levison <ladar@lavabit.com>
2018-06-05 06:27:10 -0500

    Update and then reboot OpenBSD builds gracefully.

--- scripts/openbsd6/pkg.sh
+++ scripts/openbsd6/pkg.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+# Setup a package mirror.
 echo "https://ftp.usa.openbsd.org/pub/OpenBSD/" > /etc/installurl
+
+# Update the system.
+pkg_add -u u
+
+# Install a few basic tools.
 pkg_add -I curl wget bash sudo-- vim--no_x11
 
 # Since most scripts expect bash to be in the bin directory, create a symlink.
 ln -s /usr/local/bin/bash /bin/bash
 
+# Some hypervisors require this to run OpenBSD properly.
 echo "kern.allowkmem=1" > /etc/sysctl.conf
-reboot
+
+# Reboot gracefully.
+shutdown -r +1
