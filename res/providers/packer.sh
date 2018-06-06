b86e55cbf0afa6f4de101f1ef95c5c84867fa280
Ladar Levison <ladar@lavabit.com>
2018-06-06 00:10:51 -0500

    Minor cleanup for the packer build script.

--- res/providers/packer.sh
+++ res/providers/packer.sh
@@ -3,25 +3,29 @@
 export GOPATH=$HOME/go/
 
 # Fetch gox
-go get github.com/mitchellh/gox && cd $GOPATH/src/github.com/mitchellh/gox 
+go get github.com/mitchellh/gox && cd $GOPATH/src/github.com/mitchellh/gox
 go build -o bin/gox .
 
 # Fetch packer
 go get github.com/hashicorp/packer && cd $GOPATH/src/github.com/hashicorp/packer
+
+# For now, stick to the 1.2.4 version, as the 1.3.0 is too buggy.
 git checkout v1.2.4
 
 # Customize
 sed -i -e "s/common.Retry(10, 10, 3/common.Retry(10, 300, 32/g" post-processor/vagrant-cloud/step_upload.go
 
-# Build for Linux
-#go build -o bin/packer .
-
-# Build for Windows and Linux
+# Build for Linux, Darwin, and Windows
 PATH=$GOPATH/bin:$PATH
-XC_ARCH=amd64 XC_OS="windows darwin linux" scripts/build.sh 
+XC_ARCH=amd64 XC_OS="windows darwin linux" scripts/build.sh
 
 # Install
-sudo mv pkg/linux_amd64/packer /usr/local/bin/
+sudo install pkg/linux_amd64/packer /usr/local/bin/
 sudo chown root:root /usr/local/bin/packer
 sudo chcon unconfined_u:object_r:bin_t:s0 /usr/local/bin/packer
 
+# Results
+printf "\n\nBinaries\n"
+printf "  $HOME/go/src/github.com/hashicorp/packer/pkg/linux_amd64/packer\n"
+printf "  $HOME/go/src/github.com/hashicorp/packer/pkg/darwin_amd64/packer\n"
+printf "  $HOME/go/src/github.com/hashicorp/packer/pkg/windows_amd64/packer\n\n\n"
