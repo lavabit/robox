5720532f6ae55bb283080419ab88c0a04d960725
Ladar Levison <ladar@lavabit.com>
2018-06-08 00:58:22 -0500

    Version bumped.

--- robox.sh
+++ robox.sh
@@ -9,7 +9,7 @@
 # OpenBSD needs guest agent install scripts.
 
 # Version Information
-export VERSION="1.6.16"
+export VERSION="1.6.18"
 export AGENT="Vagrant/2.0.0 (+https://www.vagrantup.com; ruby2.3.4):"
 
 # Limit the number of cpus packer will use.
