42c632ce48e5567376c5f30169de4a43e5544b48
Ladar Levison <ladar@lavabit.com>
2018-06-08 00:21:41 -0500

    Adding the roboxes tag.

---
+++ tpl/roboxes-default.rb
@@ -0,0 +1,54 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+
+  # config.vm.box = "roboxes/bazinga"
+  # config.vm.hostname = "bazinga.roboxes"
+  config.vm.synced_folder ".", "/vagrant", disabled: true
+
+  config.vm.box_check_update = true
+  config.vm.box_download_checksum = true
+  config.vm.box_download_checksum_type = "sha256"
+
+  # config.vm.provision "shell", run: "always", inline: <<-SHELL
+  # SHELL
+
+  # Adding a second CPU and increasing the RAM to 2048MB will speed
+  # things up considerably should you decide to do anythinc with this box.
+  config.vm.provider :hyperv do |v, override|
+    v.maxmemory = 2048
+    v.memory = 2048
+    v.cpus = 2
+  end
+
+  config.vm.provider :libvirt do |v, override|
+    v.disk_bus = "scsi"
+    v.driver = "kvm"
+    v.video_vram = 256
+    v.memory = 2048
+    v.cpus = 2
+  end
+
+  config.vm.provider :parallels do |v, override|
+    v.memory = 2048
+    v.cpus = 2
+  end
+
+  config.vm.provider :virtualbox do |v, override|
+    v.customize ["modifyvm", :id, "--memory", 2048]
+    v.customize ["modifyvm", :id, "--vram", 256]
+    v.customize ["modifyvm", :id, "--cpus", 2]
+    v.gui = false
+  end
+
+  ["vmware_fusion", "vmware_workstation", "vmware_desktop"].each do |provider|
+    config.vm.provider provider do |v, override|
+      v.vmx["cpuid.coresPerSocket"] = "1"
+      v.vmx["memsize"] = "2048"
+      v.vmx["numvcpus"] = "2"
+      v.gui = false
+    end
+  end
+
+end
