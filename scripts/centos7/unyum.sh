af7b4bc62906a1cf26794c6f91a7d9455120c1b9
Ladar Levison <ladar@lavabit.com>
2018-06-11 18:30:06 -0500

    Updated kernel mirror URL for CentOS.

--- scripts/centos7/unyum.sh
+++ scripts/centos7/unyum.sh
@@ -2,9 +2,9 @@
 
 sed -i -e "s/^baseurl/#baseurl/g" /etc/yum.repos.d/CentOS-Base.repo
 sed -i -e "s/^#mirrorlist/mirrorlist/g" /etc/yum.repos.d/CentOS-Base.repo
-sed -i -e "s/https:\/\/mirrors.kernel.org\/centos\//http:\/\/mirror.centos.org\/centos\//g" /etc/yum.repos.d/CentOS-Base.repo
+sed -i -e "s/https:\/\/mirrors.edge.kernel.org\/centos\//http:\/\/mirror.centos.org\/centos\//g" /etc/yum.repos.d/CentOS-Base.repo
 
 sed -i -e "s/^baseurl/#baseurl/g" /etc/yum.repos.d/epel.repo 
 sed -i -e "s/^#mirrorlist/mirrorlist/g" /etc/yum.repos.d/epel.repo 
-sed -i -e "s/https:\/\/mirrors.kernel.org\/fedora-epel\//http:\/\/download.fedoraproject.org\/pub\/epel\//g" /etc/yum.repos.d/epel.repo 
+sed -i -e "s/https:\/\/mirrors.edge.kernel.org\/fedora-epel\//http:\/\/download.fedoraproject.org\/pub\/epel\//g" /etc/yum.repos.d/epel.repo 
 
