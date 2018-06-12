6b0ea43ed4d9e0a112ec2c26ad2fa4521b660c5f
Ladar Levison <ladar@lavabit.com>
2018-06-11 20:54:53 -0500

    Adding Alpine 3.8 pre-release configuration.

--- scripts/centos7/yum.sh
+++ scripts/centos7/yum.sh
@@ -7,8 +7,9 @@ sed -i -e "s/http:\/\/mirror.centos.org\/centos\//https:\/\/mirrors.edge.kernel.
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 yum --quiet --assumeyes --enablerepo=extras install epel-release
 
-sed -i -e "s/^#baseurl/baseurl/g" /etc/yum.repos.d/epel.repo 
-sed -i -e "s/^mirrorlist/#mirrorlist/g" /etc/yum.repos.d/epel.repo 
-sed -i -e "s/http:\/\/download.fedoraproject.org\/pub\/epel\//https:\/\/mirrors.edge.kernel.org\/fedora-epel\//g" /etc/yum.repos.d/epel.repo 
+sed -i -e "s/^#baseurl/baseurl/g" /etc/yum.repos.d/epel.repo
+sed -i -e "s/^mirrorlist/#mirrorlist/g" /etc/yum.repos.d/epel.repo
+# sed -i -e "s/http:\/\/download.fedoraproject.org\/pub\/epel\//https:\/\/mirrors.edge.kernel.org\/fedora-epel\//g" /etc/yum.repos.d/epel.repo
+sed -i -e "s/http:\/\/download.fedoraproject.org\/pub\/epel\//https:\/\/mirrors.kernel.org\/fedora-epel\//g" /etc/yum.repos.d/epel.repo
 
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
