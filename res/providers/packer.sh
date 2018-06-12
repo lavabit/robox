4e15375daea5422bb582ea3cc1216d8ef1adb5b2
Ladar Levison <ladar@lavabit.com>
2018-06-12 05:37:26 -0500

    Adding the split function patch, and updated the packer build script.

--- res/providers/packer.sh
+++ res/providers/packer.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Cross Platform Script Directory
+pushd `dirname $0` > /dev/null
+BASE=`pwd -P`
+popd > /dev/null
+cd $BASE
+
 export GOPATH=$HOME/go/
+
+# Cleanup any existing go directories.
+rm -rf $GOPATH
 
 # Fetch gox
 go get github.com/mitchellh/gox && cd $GOPATH/src/github.com/mitchellh/gox
@@ -9,16 +18,19 @@ go build -o bin/gox .
 # Fetch packer
 go get github.com/hashicorp/packer && cd $GOPATH/src/github.com/hashicorp/packer
 
+# Apply the split function patch.
+cat $BASE/packer-split-function.patch | patch -p1
+
 # For now, stick to the 1.2.4 version, as the 1.3.0 is too buggy.
-git checkout v1.2.4
+# git checkout v1.2.4
 
 # Customize
-sed -i -e "s/common.Retry(10, 10, 3/common.Retry(10, 300, 32/g" post-processor/vagrant-cloud/step_upload.go
+sed -i -e "s/common.Retry(10, 10, 3/common.Retry(10, 300, 8/g" post-processor/vagrant-cloud/step_upload.go
 
 # Build for Linux, Darwin, and Windows
 PATH=$GOPATH/bin:$PATH
 XC_ARCH=amd64 XC_OS="windows darwin linux" scripts/build.sh
-
+exit
 # Install
 sudo install pkg/linux_amd64/packer /usr/local/bin/
 sudo chown root:root /usr/local/bin/packer
