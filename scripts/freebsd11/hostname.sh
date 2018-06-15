161627c88dc11445031af555c288f8990f459acf
Ladar Levison <ladar@lavabit.com>
2018-06-15 10:18:37 -0500

    BSD hostname updates.

--- scripts/freebsd11/hostname.sh
+++ scripts/freebsd11/hostname.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -eux
 
 if [[ "$PACKER_BUILD_NAME" =~ ^magma-freebsd11-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
+  sed -i -e "s/hostname=\".*\"/hostname=\"magma.localdomain\"/g" /etc/defaults/rc.conf-e
   sed -i -e "s/hostname=\".*\"/hostname=\"magma.localdomain\"/g" /etc/defaults/rc.conf
+  printf "\n127.0.0.1    magma magma.localdomain\n" >> /etc/hosts
   hostname magma.localdomain
 elif [[ "$PACKER_BUILD_NAME" =~ ^generic-freebsd11-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
-  sed -i -e "s/hostname=\".*\"/hostname=\"freebsd.localdomain\"/g" /etc/defaults/rc.conf
-  hostname freebsd.localdomain
+  sed -i -e "s/hostname=\".*\"/hostname=\"freebsd11.localdomain\"/g" /etc/defaults/rc.conf-e
+  sed -i -e "s/hostname=\".*\"/hostname=\"freebsd11.localdomain\"/g" /etc/defaults/rc.conf
+  printf "\n127.0.0.1    freebsd11 freebsd11.localdomain\n" >> /etc/hosts
+  hostname freebsd11.localdomain
 else
+  sed -i -e "s/hostname=\".*\"/hostname=\"bazinga.localdomain\"/g" /etc/defaults/rc.conf-e
   sed -i -e "s/hostname=\".*\"/hostname=\"bazinga.localdomain\"/g" /etc/defaults/rc.conf
+  printf "\n127.0.0.1    bazinga bazinga.localdomain\n" >> /etc/hosts
   hostname bazinga.localdomain
 fi
--- scripts/openbsd6/hostname.sh
+++ scripts/openbsd6/hostname.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -eux
 
-if [[ "$PACKER_BUILD_NAME" =~ ^magma-openbsd-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
+if [[ "$PACKER_BUILD_NAME" =~ ^magma-openbsd6-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
+  printf "\n127.0.0.1    magma magma.localdomain\n" >> /etc/hosts
   echo "magma.localdomain" > /etc/myname
   hostname magma.localdomain
-elif [[ "$PACKER_BUILD_NAME" =~ ^generic-openbsd-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
-  echo "openbsd.localdomain" > /etc/myname
-  hostname openbsd.localdomain
+elif [[ "$PACKER_BUILD_NAME" =~ ^generic-openbsd6-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
+  printf "\n127.0.0.1    openbsd6 openbsd6.localdomain\n" >> /etc/hosts
+  echo "openbsd6.localdomain" > /etc/myname
+  hostname openbsd6.localdomain
 else
+  printf "\n127.0.0.1    bazinga bazinga.localdomain\n" >> /etc/hosts
   echo "bazinga.localdomain" > /etc/myname
   hostname bazinga.localdomain
 fi
