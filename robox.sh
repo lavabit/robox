b54d09146754516d523107da56f62725ff8c7fa2
Ladar Levison <ladar@lavabit.com>
2018-07-12 21:37:16 -0500

    Fedora 25/26 and Gentoo URLs updated.

--- robox.sh
+++ robox.sh
@@ -47,8 +47,8 @@ FILES="magma-docker.json "\
 "developer-hyperv.json developer-vmware.json developer-libvirt.json developer-virtualbox.json"
 
 # Collect the list of ISO urls.
-ISOURLS=(`grep -E "iso_url|guest_additions_url" $FILES | grep -v -E "res/media/ubuntu-18.10-server-amd64.iso|res/media/rhel-server-6.9-x86_64-dvd.iso|res/media/rhel-server-7.5-x86_64-dvd.iso" | awk -F'"' '{print $4}'`)
-ISOSUMS=(`grep -E "iso_checksum|guest_additions_sha256" $FILES | grep -v "iso_checksum_type" | grep -v -E "4836e45bda018a3d99aca40599150735e15a070f21ee287dead360acff2841b4|3f961576e9f81ea118566f73f98d7bdf3287671c35436a13787c1ffd5078cf8e|d0dd6ae5e001fb050dafefdfd871e7e648b147fb2d35f0e106e0b34a0163e8f5" | awk -F'"' '{print $4}'`)
+ISOURLS=(`grep -E "iso_url|guest_additions_url" $FILES | grep -v -E "res/media/ubuntu-18.10-server-amd64.iso|res/media/rhel-server-6.10-x86_64-dvd.iso|res/media/rhel-server-7.5-x86_64-dvd.iso" | awk -F'"' '{print $4}'`)
+ISOSUMS=(`grep -E "iso_checksum|guest_additions_sha256" $FILES | grep -v "iso_checksum_type" | grep -v -E "4836e45bda018a3d99aca40599150735e15a070f21ee287dead360acff2841b4|1e15f9202d2cdd4b2bdf9d6503a8543347f0cb8cc06ba9a0dfd2df4fdef5c727|d0dd6ae5e001fb050dafefdfd871e7e648b147fb2d35f0e106e0b34a0163e8f5" | awk -F'"' '{print $4}'`)
 
 # Collect the list of box names.
 MAGMA_BOXES=`grep -E '"name":' $FILES | awk -F'"' '{print $4}' | grep "magma-" | sort --field-separator=- -k 3i -k 2.1,2.0`
@@ -182,7 +182,7 @@ function verify_url {
 function verify_sum {
 
   # Grab just the response header and look for the 200 response code to indicate the link is valid.
-  curl --silent --head "$1" | head -1 | grep --silent --extended-regexp "HTTP/1\.1 200 OK|HTTP/2\.0 200 OK"
+  curl --silent --location --head "$1" | head -1 | grep --silent --extended-regexp "HTTP/1\.1 200 OK|HTTP/2\.0 200 OK"
 
   # The grep return code tells us whether it found a match in the header or not.
   if [ $? != 0 ]; then
@@ -191,11 +191,11 @@ function verify_sum {
   fi
 
   # Grab the ISO and pipe the data through sha256sum, then compare the checksum value.
-  curl --silent "$1" | sha256sum | grep --silent "$2"
+  SUM=`curl --silent --location "$1" | sha256sum | tr -d '  -'`
+  echo $SUM | grep --silent "$2"
 
   # The grep return code tells us whether it found a match in the header or not.
   if [ $? != 0 ]; then
-    SUM=`curl --silent "$1" | sha256sum | awk -F' ' '{print $1}'`
     printf "Hash Failure:  $1\n"
     printf "Found       -  $SUM\n"
     printf "Expected    -  $2\n\n"
@@ -538,7 +538,7 @@ function public() {
 
 function localized() {
   verify_local 746586112 4836e45bda018a3d99aca40599150735e15a070f21ee287dead360acff2841b4 res/media/ubuntu-18.10-server-amd64.iso http://cdimage.ubuntu.com/ubuntu-server/daily/20180611/cosmic-server-amd64.iso
-  verify_local 3887071232 3f961576e9f81ea118566f73f98d7bdf3287671c35436a13787c1ffd5078cf8e res/media/rhel-server-6.9-x86_64-dvd.iso https://archive.org/download/rhel-server-6.9-x86_64-dvd/rhel-server-6.9-x86_64-dvd.iso
+  # verify_local 3895459840 1e15f9202d2cdd4b2bdf9d6503a8543347f0cb8cc06ba9a0dfd2df4fdef5c727 res/media/rhel-server-6.10-x86_64-dvd.iso https://archive.org/download/rhel-server-6.10-x86_64-dvd/rhel-server-6.10-x86_64-dvd.iso
   verify_local 4617928704 d0dd6ae5e001fb050dafefdfd871e7e648b147fb2d35f0e106e0b34a0163e8f5 res/media/rhel-server-7.5-x86_64-dvd.iso https://archive.org/download/rhel-server-7.5-x86_64-dvd/rhel-server-7.5-x86_64-dvd.iso
 }
 
