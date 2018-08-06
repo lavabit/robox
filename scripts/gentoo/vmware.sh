5510f1f6fd8c49f0fce633442be37938c58706e3
Ladar Levison <ladar@lavabit.com>
2018-08-06 14:35:49 -0500

    Added a mask which will exclude broken OpenSSH version for Gentoo builds running atop VMWare.

--- scripts/gentoo/vmware.sh
+++ scripts/gentoo/vmware.sh
@@ -7,7 +7,18 @@ if [[ `dmidecode -s system-product-name` != "VMware Virtual Platform" ]]; then
     exit 0
 fi
 
-USE="-X -resolutionkms icu pam ssl fuse vgauth xml-security-c grabbitmqproxy python_targets_python3_6" emerge --ask=n --autounmask-write=y --autounmask-continue=y app-emulation/open-vm-tools =openssh-7.5_p1-r4
+# VMWare NAT configurations don't work with the latest version of OpenSSH (currently v7.7),
+# so we force emerge to use install OpenSSH v7.5. If you want to use the latest version of
+# OpenSSH, set ssh_info_public as true in VMWare provider section of the Vagrantfile.
+cat <<-EOF > /etc/portage/package.mask/openssh
+>=net-misc/openssh-7.6
+EOF
+
+cat <<-EOF > /etc/portage/package.unmask/openssh
+=net-misc/openssh-7.5
+EOF
+
+USE="-X -resolutionkms icu pam ssl fuse vgauth xml-security-c grabbitmqproxy python_targets_python3_6" emerge --ask=n --autounmask-write=y --autounmask-continue=y app-emulation/open-vm-tools net-misc/openssh
 
 # Perform any configuration file updates.
 etc-update --automode -5
@@ -18,4 +29,3 @@ rc-service vmware-tools start
 # systemctl start vmtoolsd.service
 
 rm --force /root/linux.iso
-
