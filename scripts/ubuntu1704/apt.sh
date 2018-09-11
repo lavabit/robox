23b15fd476a9b7ff37f5eff499ee531939b0befc
Ladar Levison <ladar@lavabit.com>
2018-09-11 15:03:38 -0500

    We're just going to replace the entire sources.list file.

--- scripts/ubuntu1704/apt.sh
+++ scripts/ubuntu1704/apt.sh
@@ -27,6 +27,62 @@ printf "APT::Periodic::Enable \"0\";\n" >> /etc/apt/apt.conf.d/10periodic
 systemctl stop apt-daily.service apt-daily.timer
 systemctl stop snapd.service snapd.socket snapd.refresh.timer
 
+# Overwrite the sources.list file.
+cat <<-EOF > /etc/apt/sources.list
+# deb cdrom:[Ubuntu-Server 17.04 _Zesty Zapus_ - Release amd64 (20170412)]/ zesty main restricted
+# deb cdrom:[Ubuntu-Server 17.04 _Zesty Zapus_ - Release amd64 (20170412)]/ zesty main restricted
+
+# See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
+# newer versions of the distribution.
+deb http://old-releases.ubuntu.com/ubuntu zesty main restricted
+# deb-src http://old-releases.ubuntu.com/ubuntu zesty main restricted
+
+## Major bug fix updates produced after the final release of the
+## distribution.
+deb http://old-releases.ubuntu.com/ubuntu zesty-updates main restricted
+# deb-src http://old-releases.ubuntu.com/ubuntu zesty-updates main restricted
+
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
+## team. Also, please note that software in universe WILL NOT receive any
+## review or updates from the Ubuntu security team.
+deb http://old-releases.ubuntu.com/ubuntu zesty universe
+# deb-src http://old-releases.ubuntu.com/ubuntu zesty universe
+deb http://old-releases.ubuntu.com/ubuntu zesty-updates universe
+# deb-src http://old-releases.ubuntu.com/ubuntu zesty-updates universe
+
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
+## team, and may not be under a free licence. Please satisfy yourself as to
+## your rights to use the software. Also, please note that software in
+## multiverse WILL NOT receive any review or updates from the Ubuntu
+## security team.
+deb http://old-releases.ubuntu.com/ubuntu zesty multiverse
+# deb-src http://old-releases.ubuntu.com/ubuntu zesty multiverse
+deb http://old-releases.ubuntu.com/ubuntu zesty-updates multiverse
+# deb-src http://old-releases.ubuntu.com/ubuntu zesty-updates multiverse
+
+## N.B. software from this repository may not have been tested as
+## extensively as that contained in the main release, although it includes
+## newer versions of some applications which may provide useful features.
+## Also, please note that software in backports WILL NOT receive any review
+## or updates from the Ubuntu security team.
+deb http://old-releases.ubuntu.com/ubuntu zesty-backports main restricted universe multiverse
+# deb-src http://old-releases.ubuntu.com/ubuntu zesty-backports main restricted universe multiverse
+
+## Uncomment the following two lines to add software from Canonical's
+## 'partner' repository.
+## This software is not part of Ubuntu, but is offered by Canonical and the
+## respective vendors as a service to Ubuntu users.
+# deb http://archive.canonical.com/ubuntu zesty partner
+# deb-src http://archive.canonical.com/ubuntu zesty partner
+
+# deb http://security.ubuntu.com/ubuntu zesty-security main restricted
+# deb http://security.ubuntu.com/ubuntu zesty-security main restricted
+# deb http://security.ubuntu.com/ubuntu zesty-security universe
+# deb http://security.ubuntu.com/ubuntu zesty-security universe
+# deb http://security.ubuntu.com/ubuntu zesty-security multiverse
+# deb http://security.ubuntu.com/ubuntu zesty-security multiverse
+EOF
+
 # Update the package database.
 apt-get --assume-yes -o Dpkg::Options::="--force-confnew" update; error
 
