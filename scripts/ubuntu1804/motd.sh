d3675df8f6f0c77d018136e50c0ed739837baea5
Ladar Levison <ladar@lavabit.com>
2018-09-26 00:20:50 -0500

    Ensure the correct motd is printed for Lineage and generic Ubuntu builds.

--- scripts/ubuntu1804/motd.sh
+++ scripts/ubuntu1804/motd.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -eux
 
-sed -i -e "s/motd=\/run\/motd.dynamic/motd=\/etc\/motd/g" /etc/pam.d/sshd 
-sed -i -e "s/motd=\/run\/motd.dynamic/motd=\/etc\/motd/g" /etc/pam.d/login
+sed -i -e "s/motd=\/run\/motd.dynamic/motd=\/etc\/motd/g" /etc/pam.d/sshd
+sed -i -e "s/\(.*pam_motd.so.*noupdate.*\)/# \1/g" /etc/pam.d/sshd
 
-cat << EOF > /etc/motd
+sed -i -e "s/motd=\/run\/motd.dynamic/motd=\/etc\/motd/g" /etc/pam.d/login
+sed -i -e "s/\(.*pam_motd.so.*noupdate.*\)/# \1/g" /etc/pam.d/loginc/pam.d/login
+
+cat <<-EOF > /etc/motd
 EOF
