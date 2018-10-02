c9bee8c205cbc0d235e1af218434c426b1b0cf3f
Ladar Levison <ladar@lavabit.com>
2018-10-01 19:15:47 -0500

    Updated the Ubuntu motd logic.

--- scripts/ubuntu1810/motd.sh
+++ scripts/ubuntu1810/motd.sh
@@ -6,5 +6,15 @@ sed -i -e "s/\(.*pam_motd.so.*noupdate.*\)/# \1/g" /etc/pam.d/sshd
 sed -i -e "s/motd=\/run\/motd.dynamic/motd=\/etc\/motd/g" /etc/pam.d/login
 sed -i -e "s/\(.*pam_motd.so.*noupdate.*\)/# \1/g" /etc/pam.d/loginc/pam.d/login
 
+mkdir -p /root/.cache/
+touch /root/.cache/motd.legal-displayed
+
+if [ -d /home/vagrant/ ]; then
+  mkdir -p /home/vagrant/.cache/
+  touch /home/vagrant/.cache/motd.legal-displayed
+  chown vagrant:vagrant /home/vagrant/.cache/ 
+  chown vagrant:vagrant /home/vagrant/.cache/motd.legal-displayed
+fi
+
 cat <<-EOF > /etc/motd
 EOF
