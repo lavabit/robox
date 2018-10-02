30c71ec2bd39ec7a1af62c08685f09d79ca8775d
Ladar Levison <ladar@lavabit.com>
2018-10-02 04:14:45 -0500

    Fixing typo in Ubuntu motd scripts.

--- scripts/ubuntu1610/motd.sh
+++ scripts/ubuntu1610/motd.sh
@@ -4,7 +4,7 @@ sed -i -e "s/motd=\/run\/motd.dynamic/motd=\/etc\/motd/g" /etc/pam.d/sshd
 sed -i -e "s/\(.*pam_motd.so.*noupdate.*\)/# \1/g" /etc/pam.d/sshd
 
 sed -i -e "s/motd=\/run\/motd.dynamic/motd=\/etc\/motd/g" /etc/pam.d/login
-sed -i -e "s/\(.*pam_motd.so.*noupdate.*\)/# \1/g" /etc/pam.d/loginc/pam.d/login
+sed -i -e "s/\(.*pam_motd.so.*noupdate.*\)/# \1/g" /etc/pam.d/login
 
 mkdir -p /root/.cache/
 touch /root/.cache/motd.legal-displayed
