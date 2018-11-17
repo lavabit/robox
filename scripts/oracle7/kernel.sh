6aebb5ac8d253ae7c71a7287a242674c1d212b94
Ladar Levison <ladar@lavabit.com>
2018-11-17 04:19:53 -0600

    Improved the Oracle 7 kernel script, in attempt to fix VirtualBox build.

--- scripts/oracle7/kernel.sh
+++ scripts/oracle7/kernel.sh
@@ -9,4 +9,21 @@ fi
 # Now that the system is running atop the updated kernel, we can install the
 # development files for the kernel. These files are required to compile the
 # virtualization kernel modules later in the provisioning process.
-yum --quiet --assumeyes install kernel-tools kernel-devel kernel-headers
+yum --quiet --assumeyes install kernel-tools kernel-devel kernel-headers kernel-uek-devel
+
+# Dump a list of installed kernel packages for the log file.
+printf "\n------------------------------------------------------\n" && \
+rpm --query --all | grep kernel | sort && \
+printf -- "------------------------------------------------------\n"
+
+# Remove the duplicate UEK firmware packatges first.
+PACKAGES=`rpm --query --last kernel-uek-firmware | awk -F' ' '{print $1}' | tail --lines=+2`
+if [ ! -z $PACKAGES ]; then
+  rpm --erase $PACKAGES
+fi
+
+# Then remove the duplicate UEK kernel packages.
+PACKAGES=`rpm --query --last kernel-uek | awk -F' ' '{print $1}' | tail --lines=+2`
+if [ ! -z $PACKAGES ]; then
+  rpm --erase $PACKAGES
+fi
