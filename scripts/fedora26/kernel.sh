bc8ba6d234e1de63d85e9f9f3b554391963e0df4
Ladar Levison <ladar@lavabit.com>
2018-11-27 10:37:42 -0600

    Updated the Fedora kernel script options.

--- scripts/fedora26/kernel.sh
+++ scripts/fedora26/kernel.sh
@@ -10,10 +10,10 @@ error() {
 # Now that the system is running atop the updated kernel, we can install the
 # development files for the kernel. These files are required to compile the
 # virtualization kernel modules later in the provisioning process.
-dnf install --assumeyes kernel-tools kernel-devel kernel-headers; error
+dnf --assumeyes install kernel-tools kernel-devel kernel-headers; error
 
 # Now that the system is running on the updated kernel, we can remove the
 # old kernel(s) from the system.
 if [[ `rpm -q kernel | wc -l` != 1 ]]; then
-  dnf --setopt=protected_packages remove $(dnf repoquery --installonly --latest-limit=-1 -q); error
+  dnf --setopt=protected_packages= --assumeyes remove $(dnf repoquery --installonly --latest-limit=-1 -q); error
 fi
