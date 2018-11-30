698f03ad6a3dad31aa6317e60fed4dfa243c6a80
Ladar Levison <ladar@lavabit.com>
2018-11-29 23:11:44 -0600

    Modified the upload script to parse the necessary box parameters from the file name.

--- res/scripts/upload.sh
+++ res/scripts/upload.sh
@@ -12,11 +12,15 @@ pushd `dirname $CMD` > /dev/null
 BASE=`pwd -P`
 popd > /dev/null
 
-ORG="$1"
-NAME="$2"
-PROVIDER="$3"
-VERSION="$4"
-FILE="$5"
+if [ $# != 1 ]; then
+  tput setaf 1; printf "\n\n  Usage:\n    $0 FILENAME\n\n\n"; tput sgr0
+  exit 1
+fi
+
+if [ ! -f "$1" ]; then
+  tput setaf 1; printf "\n\nThe $1 file does not exist.\n\n\n"; tput sgr0
+  exit 1
+fi
 
 if [ -f /opt/vagrant/embedded/bin/curl ]; then
   export CURL="/opt/vagrant/embedded/bin/curl"
@@ -57,6 +61,62 @@ if [ -z ${VAGRANT_CLOUD_TOKEN} ]; then
   exit 2
 fi
 
+FILENAME=`basename "$1"`
+FILEPATH=`realpath "$1"`
+
+ORG=`echo "$FILENAME" | sed "s/\([a-z]*\)[\-]*\([a-z0-9-]*\)-\(hyperv\|vmware\|libvirt\|docker\|parallels\|virtualbox\)-\([0-9\.]*\).box/\1/g"`
+BOX=`echo "$FILENAME" | sed "s/\([a-z]*\)[-]*\([a-z0-9-]*\)-\(hyperv\|vmware\|libvirt\|docker\|parallels\|virtualbox\)-\([0-9\.]*\).box/\2/g"`
+PROVIDER=`echo "$FILENAME" | sed "s/\([a-z]*\)[-]*\([a-z0-9-]*\)-\(hyperv\|vmware\|libvirt\|docker\|parallels\|virtualbox\)-\([0-9\.]*\).box/\3/g"`
+VERSION=`echo "$FILENAME" | sed "s/\([a-z]*\)[-]*\([a-z0-9-]*\)-\(hyperv\|vmware\|libvirt\|docker\|parallels\|virtualbox\)-\([0-9\.]*\).box/\4/g"`
+
+# Handle the Lavabit boxes.
+if [ "$ORG" == "magma" ]; then
+  ORG="lavabit"
+  if [ "$BOX" == "" ]; then
+    BOX="magma"
+  else
+    BOX="magma-$BOX"
+  fi
+
+  # Specialized magma box name mappings.
+  [ "$BOX" == "magma-alpine36" ] && BOX="magma-alpine"
+  [ "$BOX" == "magma-debian8" ] && BOX="magma-debian"
+  [ "$BOX" == "magma-fedora27" ] && BOX="magma-fedora"
+  [ "$BOX" == "magma-freebsd11" ] && BOX="magma-freebsd"
+  [ "$BOX" == "magma-openbsd6" ] && BOX="magma-openbsd"
+
+fi
+
+# Handle the Lineage boxes.
+if [ "$ORG" == "lineage" ] || [ "$ORG" == "lineageos" ]; then
+  if [ "$BOX" == "" ]; then
+    BOX="lineage"
+  else
+    BOX="lineage-$BOX"
+  fi
+fi
+
+# Verify the values were all parsed properly.
+if [ "$ORG" == "" ]; then
+  tput setaf 1; printf "\n\nThe organization couldn't be parsed from the file name.\n\n\n"; tput sgr0
+  exit 1
+fi
+
+if [ "$BOX" == "" ]; then
+  tput setaf 1; printf "\n\nThe box name couldn't be parsed from the file name.\n\n\n"; tput sgr0
+  exit 1
+fi
+
+if [ "$PROVIDER" == "" ]; then
+  tput setaf 1; printf "\n\nThe provider couldn't be parsed from the file name.\n\n\n"; tput sgr0
+  exit 1
+fi
+
+if [ "$VERSION" == "" ]; then
+  tput setaf 1; printf "\n\nThe version couldn't be parsed from the file name.\n\n\n"; tput sgr0
+  exit 1
+fi
+
 printf "\n\n"
 
 tput setaf 5; printf "Create the version.\n"; tput sgr0
@@ -67,7 +127,7 @@ ${CURL} \
   --retry-delay 60 \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-  "https://app.vagrantup.com/api/v1/box/$ORG/$NAME/versions" \
+  "https://app.vagrantup.com/api/v1/box/$ORG/$BOX/versions" \
   --data "
     {
       \"version\": {
@@ -86,7 +146,7 @@ ${CURL} \
   --retry-delay 60 \
   --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
   --request DELETE \
-  https://app.vagrantup.com/api/v1/box/$ORG/$NAME/version/$VERSION/provider/${PROVIDER} | jq --color-output
+  https://app.vagrantup.com/api/v1/box/$ORG/$BOX/version/$VERSION/provider/${PROVIDER} | jq --color-output
 
 printf "\n\n";
 
@@ -101,7 +161,7 @@ ${CURL} \
   --retry-delay 60 \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-  https://app.vagrantup.com/api/v1/box/$ORG/$NAME/version/$VERSION/providers \
+  https://app.vagrantup.com/api/v1/box/$ORG/$BOX/version/$VERSION/providers \
   --data "{ \"provider\": { \"name\": \"$PROVIDER\" } }" | jq --color-output
 
 printf "\n\n"
@@ -110,26 +170,26 @@ printf "\n\n"
 #   --tlsv1.2 \
 #   --silent \
 #   --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-#   https://app.vagrantup.com/api/v1/box/$ORG/$NAME/version/$VERSION/provider/$PROVIDER/upload
+#   https://app.vagrantup.com/api/v1/box/$ORG/$BOX/version/$VERSION/provider/$PROVIDER/upload
 
 tput setaf 5; printf "Retrieve the upload path."; tput sgr0
 UPLOAD_PATH=`${CURL} \
   --tlsv1.2 \
   --silent \
   --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-  https://app.vagrantup.com/api/v1/box/$ORG/$NAME/version/$VERSION/provider/$PROVIDER/upload | jq -r .upload_path`
+  https://app.vagrantup.com/api/v1/box/$ORG/$BOX/version/$VERSION/provider/$PROVIDER/upload | jq -r .upload_path`
 
 # Perform the upload, and see the bits boil.
-# ${CURL} --tlsv1.2 --include --max-time 7200 --expect100-timeout 7200 --request PUT --output "$FILE.upload.log.txt" --upload-file "$FILE" "$UPLOAD_PATH"
+# ${CURL} --tlsv1.2 --include --max-time 7200 --expect100-timeout 7200 --request PUT --output "$FILEPATH.upload.log.txt" --upload-file "$FILEPATH" "$UPLOAD_PATH"
 #
 # printf "\n-----------------------------------------------------\n"
 # tput setaf 5
-# cat "$FILE.upload.log.txt"
+# cat "$FILEPATH.upload.log.txt"
 # tput sgr0
 # printf -- "-----------------------------------------------------\n\n"
 
 if [ "$UPLOAD_PATH" == "" ] || [ "$UPLOAD_PATH" == "null" ]; then
-  printf "\n\n$FILE failed to upload...\n\n"
+  printf "\n\n$FILENAME failed to upload...\n\n"
   exit 1
 fi
 
@@ -145,8 +205,8 @@ ${CURL} --tlsv1.2 \
   --max-time 7200 \
   --expect100-timeout 7200 \
   --header "Connection: keep-alive" \
-  --write-out "\n\nFILE: $FILE\nCODE: %{http_code}\nIP: %{remote_ip}\nBYTES: %{size_upload}\nRATE: %{speed_upload}\nSETUP TIME: %{time_starttransfer}\nTOTAL TIME: %{time_total}\n\n\n" \
-  --upload-file "$FILE" "$UPLOAD_PATH"
+  --write-out "\n\nFILE: $FILENAME\nCODE: %{http_code}\nIP: %{remote_ip}\nBYTES: %{size_upload}\nRATE: %{speed_upload}\nSETUP TIME: %{time_starttransfer}\nTOTAL TIME: %{time_total}\n\n\n" \
+  --upload-file "$FILEPATH" "$UPLOAD_PATH"
 
 # Give the upload time to propagate.
 sleep 10
@@ -157,7 +217,7 @@ ${CURL} \
   --max-time 7200 \
   --connect-timeout 7200 \
   --expect100-timeout 7200 \
-  "https://app.vagrantup.com/api/v1/box/$ORG/$NAME/version/$VERSION/provider/$PROVIDER" | jq --color-output
+  "https://app.vagrantup.com/api/v1/box/$ORG/$BOX/version/$VERSION/provider/$PROVIDER" | jq --color-output
 
 printf "\n\n"
 
@@ -168,7 +228,7 @@ printf "\n\n"
 #   --tlsv1.2 \
 #   --silent \
 #   --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-#   https://app.vagrantup.com/api/v1/box/$ORG/$NAME/version/$VERSION/release \
+#   https://app.vagrantup.com/api/v1/box/$ORG/$BOX/version/$VERSION/release \
 #   --request PUT | jq  --color-output '.status,.version,.providers[]' | grep -vE "hosted|hosted_token|original_url|created_at|updated_at|\}|\{"
 #
 # printf "\n\n"
@@ -176,5 +236,5 @@ printf "\n\n"
 # Revoke a Version
 # ${CURL} \
 #   --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-#   https://app.vagrantup.com/api/v1/box/$ORG/$NAME/version/$VERSION/revoke \
+#   https://app.vagrantup.com/api/v1/box/$ORG/$BOX/version/$VERSION/revoke \
 #   --request PUT
