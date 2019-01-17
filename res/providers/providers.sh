784a923283f316ab770cf204b602091dfb026575
Ladar Levison <ladar@lavabit.com>
2019-01-16 20:37:01 -0600

    Keep the packer script updates.

--- res/providers/providers.sh
+++ res/providers/providers.sh
@@ -245,13 +245,13 @@ function provide-packer() {
   # Increase the upload timeout.
   patch -p1 < $BASE/packer-upload-timeout.patch
 
-  # Fix the Hyper-V boot dervice ordering for generation one virtual machines.
-  patch -p1 < $BASE/hyperv-boot-order.patch
+  # Fix the Hyper-V boot dervice ordering for generation one virtual machines. - MERGED
+  # patch -p1 < $BASE/hyperv-boot-order.patch
 
-  # Fox the Hyper-V SSH host value bug.
-  patch -p1 < $BASH/hyperv-ssh-host.patch
+  # Fox the Hyper-V SSH host value bug. - MERGED
+  # patch -p1 < $BASH/hyperv-ssh-host.patch
 
-  # Merged into packer version >= 1.3.4.
+  # Force Hyper-V to use a legacy network adapter. - MERGED
   # patch -p1 < $BASE/hyperv-legacy-network-adapter.patch
 
   # Build for Linux, Darwin, and Windows
