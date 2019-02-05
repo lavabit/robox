2a69321312240410f11b154e9abada6ed42b42d2
Ladar Levison <ladar@lavabit.com>
2019-02-05 14:48:52 -0600

    Corrections for the new configs.

--- scripts/alpine39/apk.sh
+++ scripts/alpine39/apk.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -eux
 
 # Configure the main repository mirrors.
-printf "https://dl-4.alpinelinux.org/alpine/v3.8/main\n" > /etc/apk/repositories
-printf "https://dl-4.alpinelinux.org/alpine/v3.8/community\n" >> /etc/apk/repositories
+printf "https://dl-4.alpinelinux.org/alpine/v3.9/main\n" > /etc/apk/repositories
+printf "https://dl-4.alpinelinux.org/alpine/v3.9/community\n" >> /etc/apk/repositories
 
 # Update the package list and then upgrade.
 apk update --no-cache
