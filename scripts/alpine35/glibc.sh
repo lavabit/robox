83d6f2a378eed07cc354760bb0ccb6e3361bdef9
Ladar Levison <ladar@lavabit.com>
2019-02-26 02:03:42 -0600

    Updated Alpine glibc revision.

--- scripts/alpine35/glibc.sh
+++ scripts/alpine35/glibc.sh
@@ -28,22 +28,22 @@ retry() {
 
 # Download the glibc Alpine packages.
 retry apk --no-cache add wget ca-certificates
-retry wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub
+retry wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
 
 # Unstable.
-retry wget --quiet https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.26-r0/glibc-2.26-r0.apk
-retry wget --quiet https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.26-r0/glibc-bin-2.26-r0.apk
-retry wget --quiet https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.26-r0/glibc-dev-2.26-r0.apk
-retry wget --quiet https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.26-r0/glibc-i18n-2.26-r0.apk
+retry wget --quiet https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.29-r0/glibc-2.29-r0.apk
+retry wget --quiet https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.29-r0/glibc-bin-2.29-r0.apk
+retry wget --quiet https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.29-r0/glibc-dev-2.29-r0.apk
+retry wget --quiet https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.29-r0/glibc-i18n-2.29-r0.apk
 
 # Ensure the compatability library has been removed, to avoid conflicts.
 retry apk del libc6-compat
 
 # Install glibc.
-retry apk add glibc-2.26-r0.apk glibc-bin-2.26-r0.apk glibc-dev-2.26-r0.apk glibc-i18n-2.26-r0.apk
+retry apk add glibc-2.29-r0.apk glibc-bin-2.29-r0.apk glibc-dev-2.29-r0.apk glibc-i18n-2.29-r0.apk
 
 # Cleanup the apk files.
-rm -f glibc-2.26-r0.apk glibc-bin-2.26-r0.apk glibc-dev-2.26-r0.apk glibc-i18n-2.26-r0.apk
+rm -f glibc-2.29-r0.apk glibc-bin-2.29-r0.apk glibc-dev-2.29-r0.apk glibc-i18n-2.29-r0.apk
 
 #  Generate the English/USA locale.
 /usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8
