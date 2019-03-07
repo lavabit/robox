cd2872593bc1069497a89f54a6dc8053189c982b
Ladar Levison <ladar@lavabit.com>
2019-03-06 19:34:23 -0600

    Remove extra retry from the HardenedBSD 12 cleanup script.

--- scripts/hardenedbsd11/cleanup.sh
+++ scripts/hardenedbsd11/cleanup.sh
@@ -1,34 +1,8 @@
 #!/bin/bash -eux
 
-retry() {
-  local COUNT=1
-  local RESULT=0
-  while [[ "${COUNT}" -le 10 ]]; do
-    [[ "${RESULT}" -ne 0 ]] && {
-      [ "`which tput 2> /dev/null`" != "" ] && tput setaf 1
-      echo -e "\n${*} failed... retrying ${COUNT} of 10.\n" >&2
-      [ "`which tput 2> /dev/null`" != "" ] && tput sgr0
-    }
-    "${@}" && { RESULT=0 && break; } || RESULT="${?}"
-    COUNT="$((COUNT + 1))"
-
-    # Increase the delay with each iteration.
-    DELAY="$((DELAY + 10))"
-    sleep $DELAY
-  done
-
-  [[ "${COUNT}" -gt 10 ]] && {
-    [ "`which tput 2> /dev/null`" != "" ] && tput setaf 1
-    echo -e "\nThe command failed 10 times.\n" >&2
-    [ "`which tput 2> /dev/null`" != "" ] && tput sgr0
-  }
-
-  return "${RESULT}"
-}
-
 # Remove orphans.
-retry pkg-static autoremove --yes
+pkg-static autoremove --yes
 
 # Clean the package cache.
-retry pkg-static clean --yes --all
+pkg-static clean --yes --all
 rm -f /var/db/pkg/repo-HardenedBSD.sqlite
