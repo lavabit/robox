97078696600693da6bd47439ad15748cecac4c37
MaxPeal <30347730+MaxPeal@users.noreply.github.com>
2019-05-04 04:56:49 +0200

    Revert "#"
    This reverts commit 518651936b1c3f7ddd35c31719e648bc1bb38e19.

--- scripts/alpine38/virtualbox.sh
+++ scripts/alpine38/virtualbox.sh
@@ -1,7 +1,4 @@
-#!/bin/sh -eux
-###!/bin/bash -eux
-# make shure it will run in any case
-
+#!/bin/bash -eux
 
 retry() {
   local COUNT=1
@@ -29,13 +26,6 @@ retry() {
   return "${RESULT}"
 }
 
-if ! grep -q '^[^#].\+alpine/.\+/community' /etc/apk/repositories; then
-    # Add community repository entry based on the "main" repo URL
-    __REPO=$(grep '^[^#].\+alpine/.\+/main\>' /etc/apk/repositories)
-    echo "${__REPO}" | sed -e 's/main/community/' >> /etc/apk/repositories
-fi
-
-
 # Ensure dmidecode is available.
 retry apk add dmidecode
 
@@ -44,56 +34,25 @@ if [[ `dmidecode -s system-product-name` != "VirtualBox" ]]; then
     exit 0
 fi
 
-if [[ "$PACKER_BUILD_NAME" =~ ^(generic|magma)-(alpine3[5-7])-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
+# Install the Virtual Box Tools from the Linux Guest Additions ISO.
+printf "Installing the Virtual Box Tools.\n"
 
-	# Install the Virtual Box Tools from the Linux Guest Additions ISO.
-	printf "Installing the Virtual Box Tools.\n"
+# Add the testing repository (which isn't available over HTTPS).
+printf "@testing http://nl.alpinelinux.org/alpine/edge/testing\n" >> /etc/apk/repositories
 
-	# Add the testing repository (which isn't available over HTTPS).
-	printf "@testing http://nl.alpinelinux.org/alpine/edge/testing\n" >> /etc/apk/repositories
+# Add the primary site.
+printf "@edge http://nl.alpinelinux.org/alpine/edge/main/\n" >> /etc/apk/repositories
 
-	# Add the edge repository entry based on the "main" repo URL.
-	printf "@edge http://nl.alpinelinux.org/alpine/edge/main/\n" >> /etc/apk/repositories
+# Update the APK cache.
+retry apk update --no-cache
 
-	# Update the APK cache.
-	retry apk update --no-cache
+# Install the VirtualBox kernel modules for guest services.
+retry apk add linux-hardened@edge virtualbox-additions-hardened@testing
 
-	# Install the VirtualBox kernel modules for guest services.
-	retry apk add linux-hardened@edge virtualbox-additions-hardened@testing
-
-	# Autoload the virtualbox kernel modules.
-	echo vboxpci >> /etc/modules
-	echo vboxdrv >> /etc/modules
-	echo vboxnetflt >> /etc/modules
-
-	rm -rf /root/VBoxVersion.txt
-	rm -rf /root/VBoxGuestAdditions.iso
-
-else
-
-	apk info --installed linux-vanilla && apkRESULT="${?}" || apkRESULT="${?}"
-	if [[ $apkRESULT == "0" ]]; then
-		retry apk add virtualbox-guest-additions virtualbox-guest-modules-vanilla
-	# Autoload the virtualbox kernel modules.
-	echo vboxguest >> /etc/modules
-	echo vboxsf >> /etc/modules
-	echo vboxvideo >> /etc/modules
-	fi
-
-	apk info --installed linux-virt && apkRESULT="${?}" || apkRESULT="${?}"
-	if [[ $apkRESULT == "0" ]]; then
-		retry apk add virtualbox-guest-additions virtualbox-guest-modules-virt
-	# Autoload the virtualbox kernel modules.
-	echo vboxguest >> /etc/modules
-	echo vboxsf >> /etc/modules
-	##echo vboxvideo >> /etc/modules
-	fi
-
-	rm -rf /root/VBoxVersion.txt
-	rm -rf /root/VBoxGuestAdditions.iso
-
-fi
-
+# Autoload the virtualbox kernel modules.
+echo vboxpci >> /etc/modules
+echo vboxdrv >> /etc/modules
+echo vboxnetflt >> /etc/modules
 
 # Read in the version number.
 # export VBOXVERSION=`cat /root/VBoxVersion.txt`
