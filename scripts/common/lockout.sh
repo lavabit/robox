d7ae84be19b23bccb3e5d76c0e222bb1f5f23c30
Ladar Levison <ladar@lavabit.com>
2019-06-20 12:56:43 +0530

    Added Alpine 3.10 detection to common scripts.

--- scripts/common/lockout.sh
+++ scripts/common/lockout.sh
@@ -16,7 +16,7 @@ elif  [[ "$PACKER_BUILD_NAME" =~ ^(generic|magma)-(netbsd8)-(vmware|hyperv|libvi
   LOCKPWD=`dd if=/dev/urandom count=128 msgfmt=quiet | md5 | awk -F' ' '{print $1}'`
   /usr/sbin/user mod -p "`pwhash $LOCKPWD`" root
 
-elif [[ "$PACKER_BUILD_NAME" =~ ^(generic|magma)-(alpine3[5-9])-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
+elif [[ "$PACKER_BUILD_NAME" =~ ^(generic|magma)-(alpine3[5-9]|alpine310)-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
 
   LOCKPWD=`dd if=/dev/urandom count=128 status=none | md5sum | awk -F' ' '{print $1}'`
   printf "$LOCKPWD\n$LOCKPWD\n" | passwd root
