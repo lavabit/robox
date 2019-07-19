b1422473c235bb0880d20a913e77e611e54f324a
Tim Schumacher <timschumi@gmx.de>
2019-07-19 20:12:13 +0200

    alpine: Remove unused testing and edge repositories

--- scripts/alpine38/virtualbox.sh
+++ scripts/alpine38/virtualbox.sh
@@ -37,15 +37,6 @@ fi
 # Install the Virtual Box Tools from the Linux Guest Additions ISO.
 printf "Installing the Virtual Box Tools.\n"
 
-# Add the testing repository (which isn't available over HTTPS).
-printf "@testing http://nl.alpinelinux.org/alpine/edge/testing\n" >> /etc/apk/repositories
-
-# Add the primary site.
-printf "@edge http://nl.alpinelinux.org/alpine/edge/main/\n" >> /etc/apk/repositories
-
-# Update the APK cache.
-retry apk update --no-cache
-
 # Install the VirtualBox kernel modules for guest services.
 retry apk add virtualbox-guest-additions virtualbox-guest-modules-virt
 
