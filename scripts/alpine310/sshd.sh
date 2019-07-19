92a715b889908d166cc68768030d75af738d2aab
Tim Schumacher <timschumi@gmx.de>
2019-07-19 16:42:17 +0200

    alpine3{9,10}: Allow SSH TCP port forwarding
    Especially libvirt guests need this, since it's their main way of
    sharing ports with the host.

--- scripts/alpine310/sshd.sh
+++ scripts/alpine310/sshd.sh
@@ -5,3 +5,6 @@ sed -i "/^#UseDNS.*/d" /etc/ssh/sshd_config
 sed -i "/^# UseDNS.*/d" /etc/ssh/sshd_config
 
 printf "\nUseDNS no\n" >> /etc/ssh/sshd_config
+
+sed -i '/AllowTcpForwarding/d' /etc/ssh/sshd_config
+printf "\nAllowTcpForwarding yes\n" >> /etc/ssh/sshd_config
