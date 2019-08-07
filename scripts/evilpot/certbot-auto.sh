955ccdea23cf79fb9c96ec77234cdbd1d7668203
klosnet <infosec@cerneer.com>
2019-08-07 06:14:10 -0500

    pleasing the git Gods

---
+++ scripts/evilpot/certbot-auto.sh
@@ -0,0 +1,1 @@
+ /opt/certbot/certbot-auto certonly --standalone --non-interactive --agree-tos --rsa-key-size 4096 --email "admin@miskc.org" -d "miskc.org,www.miskc.org,imap.miskc.org,pop.miskc.org,smtp.miskc.org"
