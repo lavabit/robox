be2731b39e953de8dfaecbb37523d18b6314025a
Ladar Levison <ladar@lavabit.com>
2019-08-19 15:07:16 +0530

    Previous commit didn't actually add the script. Closes #81.

---
+++ scripts/common/leases.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -eux
+
+# Delete the DHCP client lease files from any/all locations we know about.
+
+if [ -d /var/lib/NetworkManager/ ]; then
+  find /var/lib/NetworkManager/ -name "*.lease" -or -name "*.leases"
+  find /var/lib/NetworkManager/ -name "*.lease" -or -name "*.leases" -exec rm --force {} \;
+fi
+
+if [ -d /var/lib/dhclient/ ]; then
+  find /var/lib/dhclient/ -name "*.lease" -or -name "*.leases"
+  find /var/lib/dhclient/ -name "*.lease" -or -name "*.leases" -exec rm --force {} \;
+fi
+
+if [ -d /var/lib/dhcp/ ]; then
+  find /var/lib/dhclient/ -name "*.lease" -or -name "*.leases"
+  find /var/lib/dhclient/ -name "*.lease" -or -name "*.leases" -exec rm --force {} \;
+fi
