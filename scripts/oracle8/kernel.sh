8078eb012e8ec32f8ad028373e3f3fe2cb9d9d2c
Ladar Levison <ladar@lavabit.com>
2019-08-21 15:18:12 +0530

    Disabling UEK logic in Oracle 8 till it's needed.

--- scripts/oracle8/kernel.sh
+++ scripts/oracle8/kernel.sh
@@ -5,21 +5,27 @@
 # virtualization kernel modules later in the provisioning process.
 dnf --assumeyes install kernel-tools kernel-devel kernel-headers
 
-# Remove the duplicate UEK firmware packatges first.
-PACKAGES=`rpm --query --last kernel-uek-firmware | awk -F' ' '{print $1}' | tail --lines=+2`
-if [ ! -z $PACKAGES ]; then
-  rpm --erase $PACKAGES
-fi
+printf "\n\n\n\n" 1>&2
+rpm -qa | grep kernel | sort
+printf "\n\n\n\n" 1>&2
+dnf --enablerepo=* repolist
+printf "\n\n\n\n" 1>&2
 
-# Then remove the duplicate UEK kernel packages.
-PACKAGES=`rpm --query --last kernel-uek | awk -F' ' '{print $1}' | tail --lines=+2`
-if [ ! -z $PACKAGES ]; then
-  rpm --erase $PACKAGES
-fi
-
-# Make sure we have the right kernel-uek-devel package installed, or the VirtualBox
-# addons won't build properly.
-dnf --enablerepo=ol8_UEKR* --assumeyes install kernel-uek-devel-`uname -r`
+# # Remove the duplicate UEK firmware packatges first.
+# PACKAGES=`rpm --query --last kernel-uek-firmware | awk -F' ' '{print $1}' | tail --lines=+2`
+# if [ ! -z $PACKAGES ]; then
+#   rpm --erase $PACKAGES
+# fi
+#
+# # Then remove the duplicate UEK kernel packages.
+# PACKAGES=`rpm --query --last kernel-uek | awk -F' ' '{print $1}' | tail --lines=+2`
+# if [ ! -z $PACKAGES ]; then
+#   rpm --erase $PACKAGES
+# fi
+#
+# # Make sure we have the right kernel-uek-devel package installed, or the VirtualBox
+# # addons won't build properly.
+# dnf --enablerepo=ol8_UEKR* --assumeyes install kernel-uek-devel-`uname -r`
 
 # Now that the system is running on the updated kernel, we can remove the
 # old kernel(s) from the system.
