7d8c752b629e7c96002bac3acc8e42689c475ba3
Ladar Levison <ladar@lavabit.com>
2019-10-29 11:12:34 -0500

    Fixed Gentoo lm sensor package name.

--- scripts/gentoo/emerge.sh
+++ scripts/gentoo/emerge.sh
@@ -7,7 +7,7 @@ emerge --sync
 emerge --update --deep --newuse --with-bdeps=y @system @world
 
 # Useful tools.
-USE="-X pam_ssh tty-helpers openssl" emerge --ask=n --autounmask-write=y --autounmask-continue=y app-admin/rsyslog app-admin/sudo app-admin/sysstat app-arch/bzip2 app-arch/bzip2 app-arch/gzip app-arch/gzip app-arch/tar app-arch/xz-utils app-editors/vim app-shells/bash dev-libs/lzo net-misc/curl net-misc/iputils net-misc/openssh net-misc/wget sys-apps/baselayout sys-apps/coreutils sys-apps/diffutils sys-apps/file sys-apps/findutils sys-apps/gawk sys-apps/grep sys-apps/kbd sys-apps/less sys-apps/lm_sensors sys-apps/man-pages sys-apps/mlocate sys-apps/net-tools sys-apps/openrc sys-apps/sed sys-apps/util-linux sys-apps/which sys-apps/which sys-auth/pambase sys-auth/pam_ssh sys-libs/glibc sys-libs/pam sys-process/lsof sys-process/procps sys-process/psmisc
+USE="-X pam_ssh tty-helpers openssl" emerge --ask=n --autounmask-write=y --autounmask-continue=y app-admin/rsyslog app-admin/sudo app-admin/sysstat app-arch/bzip2 app-arch/bzip2 app-arch/gzip app-arch/gzip app-arch/tar app-arch/xz-utils app-editors/vim app-shells/bash dev-libs/lzo net-misc/curl net-misc/iputils net-misc/openssh net-misc/wget sys-apps/baselayout sys-apps/coreutils sys-apps/diffutils sys-apps/file sys-apps/findutils sys-apps/gawk sys-apps/grep sys-apps/kbd sys-apps/less sys-apps/lm-sensors sys-apps/man-pages sys-apps/mlocate sys-apps/net-tools sys-apps/openrc sys-apps/sed sys-apps/util-linux sys-apps/which sys-apps/which sys-auth/pambase sys-auth/pam_ssh sys-libs/glibc sys-libs/pam sys-process/lsof sys-process/procps sys-process/psmisc
 
 # Perform any configuration file updates.
 etc-update --automode -5
