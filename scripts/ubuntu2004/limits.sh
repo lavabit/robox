a671102f45a9550a2f50ff11ce05f36d40666768
abi <abi@grinser.de>
2020-04-13 23:57:37 +0200

    #139 Add generic-ubuntu2004-libvirt for Ubuntu 20.04 beta release

---
+++ scripts/ubuntu2004/limits.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eux
+
+# Find out how much RAM is installed, and what 50% would be in KB.
+TOTALMEM=`free -k | grep -E "^Mem:" | awk -F' ' '{print $2}'`
+HALFMEM=`echo $(($TOTALMEM/2))`
+
+# Setup the memory locking limits.
+printf "*    soft    memlock    $HALFMEM\n" > /etc/security/limits.d/50-magmad.conf
+printf "*    hard    memlock    $HALFMEM\n" >> /etc/security/limits.d/50-magmad.conf
