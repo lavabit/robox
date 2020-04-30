21fe9f4268a770a0d913d983ce3b481b7b63c241
Ladar Levison <ladar@lavabit.com>
2020-04-30 00:14:30 -0500

    Removing public SSH info flag.

--- tpl/roboxes-gentoo.rb
+++ tpl/roboxes-gentoo.rb
@@ -50,7 +50,6 @@ Vagrant.configure(2) do |config|
   ["vmware_fusion", "vmware_workstation", "vmware_desktop"].each do |provider|
     config.vm.provider provider do |v, override|
       v.whitelist_verified = true
-      v.ssh_info_public = true
       v.gui = false
       v.vmx["cpuid.coresPerSocket"] = "1"
       v.vmx["memsize"] = "2048"
