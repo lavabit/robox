cf229f83f1b40976c3cf8263665fce81b541ff2c
Ladar Levison <ladar@lavabit.com>
2020-06-03 18:58:02 -0500

    Use service restart instead of systemctl on Devuan cron jobs.

--- scripts/common/keys.sh
+++ scripts/common/keys.sh
@@ -18,6 +18,8 @@ rm -f /etc/ssh/ssh_host_ecdsa_key.pub
 rm -f /etc/ssh/ssh_host_ed25519_key
 rm -f /etc/ssh/ssh_host_ed25519_key.pub
 
-if [[ "$PACKER_BUILD_NAME" =~ ^.*(ubuntu|debian|devuan|lineage).*$ ]]; then
+if [[ "$PACKER_BUILD_NAME" =~ ^.*(ubuntu|debian|lineage).*$ ]]; then
   printf "@reboot root command bash -c 'export PATH=$PATH:/usr/sbin ; export DEBIAN_FRONTEND=noninteractive ; export DEBCONF_NONINTERACTIVE_SEEN=true ; /usr/sbin/dpkg-reconfigure openssh-server &>/dev/null ; /bin/systemctl restart ssh.service ; rm --force /etc/cron.d/keys'\n" > /etc/cron.d/keys
+elif [[ "$PACKER_BUILD_NAME" =~ ^.*(devuan).*$ ]]; then
+  printf "@reboot root command bash -c 'export PATH=$PATH:/usr/sbin ; export DEBIAN_FRONTEND=noninteractive ; export DEBCONF_NONINTERACTIVE_SEEN=true ; /usr/sbin/dpkg-reconfigure openssh-server &>/dev/null ; /usr/sbin/service ssh restart ; rm --force /etc/cron.d/keys'\n" > /etc/cron.d/keys
 fi
