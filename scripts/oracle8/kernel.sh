7a2bd0dfb74e6649944e63ac3e3035ee189c444e
Ladar Levison <ladar@lavabit.com>
2020-06-09 10:02:59 -0500

    Fix the Oracle/RHEL 8 kernel cleanup commands.

--- scripts/oracle8/kernel.sh
+++ scripts/oracle8/kernel.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -eux
 
-# Now that the system is running on the updated kernel, we can remove the
-# old kernel(s) from the system.
-dnf remove $( dnf repoquery --installonly --latest-limit -1 -q kernel )
-
 # Now that the system is running atop the updated kernel, we can install the
 # development files for the kernel. These files are required to compile the
 # virtualization kernel modules later in the provisioning process.
-dnf --assumeyes install kernel-tools kernel-devel kernel-headers
+retry dnf --assumeyes install kernel-tools kernel-devel kernel-headers
+
+# Now that the system is running on the updated kernel, we can remove the
+# old kernel(s) from the system.
+if [[ `rpm -q kernel | wc -l` != 1 ]]; then
+  dnf --assumeyes remove $( rpm -q kernel | grep -v `uname -r` )
+fi
