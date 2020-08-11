d0e7a501e8063e3237254d79410e40c7bf2c553e
Ladar Levison <ladar@lavabit.com>
2020-08-11 08:53:15 -0500

    Version bump.

--- robox.sh
+++ robox.sh
@@ -6,7 +6,7 @@
 # Description: Used to build various virtual machines using packer.
 
 # Version Information
-export VERSION="3.0.24"
+export VERSION="3.0.26"
 export AGENT="Vagrant/2.2.9 (+https://www.vagrantup.com; ruby2.6.6)"
 
 # Limit the number of cpus packer will use.
