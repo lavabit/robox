0fac6d9da202a0b38660fe29cdbd523931142509
Ladar Levison <ladar@lavabit.com>
2020-10-15 19:03:18 -0500

    Attempting to fix the Devuan reboot bug.

--- scripts/devuan2/network.sh
+++ scripts/devuan2/network.sh
@@ -67,6 +67,7 @@ printf "pre-up sleep 2\n" >> /etc/network/interfaces
 printf "nameserver 4.2.2.1\nnameserver 4.2.2.2\nnameserver 208.67.220.220\n" > /etc/resolv.conf
 
 # Reboot onto the new kernel (if applicable).
-bash -c 'sleep 30 ; /sbin/reboot' &
+service networking restart
+(sleep 30 ; /sbin/reboot) &
 echo "Rebooting in thirty seconds..."
 exit 0
