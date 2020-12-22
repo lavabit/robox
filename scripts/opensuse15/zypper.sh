c7cae7acd1c0bc6269900a4e0453f7ab8d3e5835
Ladar Levison <ladar@lavabit.com>
2020-12-22 16:55:01 -0600

    Attempt to fix OpenSUSE VirtualBox build by adding reboot after it's updated.

--- scripts/opensuse15/zypper.sh
+++ scripts/opensuse15/zypper.sh
@@ -7,7 +7,7 @@ zypper --non-interactive removelock virtualbox-guest-tools || echo "The virtualb
 zypper --non-interactive removelock virtualbox-guest-kmp-default || echo "The virtualbox-guest-kmp-defaul lock removal failed."
 
 # Remove the default installation repository.
-zypper --non-interactive removerepo "openSUSE-Leap-${version}-0"
+zypper --non-interactive removerepo "`zypper repos 1 | head -1 | awk -F':' '{print $2}' | tr -d '[:space:]'`"
 
 # Add the default repositories for this release.
 # zypper --non-interactive addrepo https://download.opensuse.org/distribution/leap/${version}/repo/oss/ openSUSE-Leap-${version}-Oss
@@ -19,7 +19,6 @@ zypper --non-interactive addrepo https://mirrors.kernel.org/opensuse/distributio
 zypper --non-interactive addrepo https://mirrors.kernel.org/opensuse/distribution/leap/${version}/repo/non-oss/ openSUSE-Leap-${version}-Non-Oss
 zypper --non-interactive addrepo https://mirrors.kernel.org/opensuse/update/leap/${version}/oss/ openSUSE-Leap-${version}-Update
 zypper --non-interactive addrepo https://mirrors.kernel.org/opensuse/update/leap/${version}/non-oss/ openSUSE-Leap-${version}-Update-Non-Oss
-
 
 # Clean out any stale cache data.
 zypper --non-interactive clean --all
@@ -34,4 +33,7 @@ zypper --non-interactive update --auto-agree-with-licenses
 zypper --non-interactive install man mlocate sysstat psmisc
 
 # Update the locate database.
-# /etc/cron.daily/mlocate.cron
+systemctl enable mlocate.timer && systemctl start mlocate.timer
+
+# Force a reboot.
+(sleep 30 ; /sbin/reboot) &
