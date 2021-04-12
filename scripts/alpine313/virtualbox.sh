6076a8dade4dccc080f5234c485f42798c722bcb
Ladar Levison <ladar@lavabit.com>
2021-04-12 13:17:27 -0500

    Reverting Alpine VirtualBox module, except for 3.13 fixes.

--- scripts/alpine313/virtualbox.sh
+++ scripts/alpine313/virtualbox.sh
@@ -38,14 +38,16 @@ fi
 printf "Installing the Virtual Box Tools.\n"
 
 # Add the edge package repo.
-printf "https://sjc.edge.kernel.org/alpine/edge/main\n" >> /etc/apk/repositories
-printf "https://sjc.edge.kernel.org/alpine/edge/community\n" >> /etc/apk/repositories
+# printf "@edge-main https://sjc.edge.kernel.org/alpine/edge/main\n" >> /etc/apk/repositories
+# printf "@edge-testing https://sjc.edge.kernel.org/alpine/edge/testing\n" >> /etc/apk/repositories
+# printf "@edge-community https://sjc.edge.kernel.org/alpine/edge/community\n" >> /etc/apk/repositories
 
 # Update the package list.
-retry apk update --no-cache
+# retry apk update --no-cache
 
 # Install the VirtualBox kernel modules for guest services.
-retry apk add virtualbox-guest-modules-virt virtualbox-guest-additions
+# retry apk add virtualbox-guest-modules-virt@edge-community virtualbox-guest-additions@edge-community
+retry apk add virtualbox-guest-additions
 
 # Autoload the virtualbox kernel modules.
 rc-update add virtualbox-guest-additions default && rc-service virtualbox-guest-additions start
