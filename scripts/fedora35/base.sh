92e1fe257b6f7728c4adf3a3d9257bb3834a18cd
Ladar Levison <ladar@lavabit.com>
2021-11-25 18:14:37 -0600

    Adding the initial Fedora 35 config.

---
+++ scripts/fedora35/base.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Ensure a nameserver is being used that won't return an IP for non-existent domain names.
+printf "nameserver 4.2.2.1\nnameserver 4.2.2.2\nnameserver 208.67.220.220\n"> /etc/resolv.conf
+
+# Ensure memcached doesn't try to use IPv6.
+if [ -f /etc/sysconfig/memcached ]; then
+  sed -i "s/[,]\?\:\:1[,]\?//g" /etc/sysconfig/memcached
+fi
+
+# Enable and start the daemons.
+systemctl enable haveged
+systemctl enable memcached
+systemctl start haveged
+systemctl start memcached
+
+# Disable IPv6 and the iptables module used to firewall IPv6.
+printf "\n\nnet.ipv6.conf.all.disable_ipv6 = 1\n" >> /etc/sysctl.conf
+
+sed -i -e "/IPV6INIT.*/d;$ a IPV6INIT=no" /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i -e "/IPV6_AUTOCONF.*/d;$ a IPV6_AUTOCONF=no" /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i -e "/IPV6_DEFROUTE.*/d;$ a IPV6_DEFROUTE=no" /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i -e "/IPV6_PEERDNS.*/d;$ a IPV6_PEERDNS=no" /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i -e "/IPV6_PEERROUTES.*/d;$ a IPV6_PEERROUTES=no" /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i -e "/IPV6FORWARDING.*/d;$ a IPV6FORWARDING=no" /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i -e "/IPV6_AUTOTUNNEL.*/d;$ a IPV6_AUTOTUNNEL=no" /etc/sysconfig/network-scripts/ifcfg-eth0
+
+
+# Ensure good DNS servers are being used.
+if [ -f /etc/sysconfig/network-scripts/ifcfg-eth0 ]; then
+  printf "DNS1=4.2.2.1\n" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+  printf "DNS2=4.2.2.2\n" >> /etc/sysconfig/network-scripts/ifcfg-eth0
+fi
+
+# Close a potential security hole.
+systemctl disable remote-fs.target
+
+# Disable kernel dumping.
+# systemctl disable kdump.service
+
+
+# Setup the python path and increase the history size.
+printf "export HISTSIZE=\"100000\"\n" > /etc/profile.d/histsize.sh
+chcon "system_u:object_r:bin_t:s0" /etc/profile.d/histsize.sh
+chmod 644 /etc/profile.d/histsize.sh
+
+# Set the timezone to Pacific time.
+printf "ZONE=\"America/Los_Angeles\"\n" > /etc/sysconfig/clock
+
+# Reboot
+shutdown --reboot --no-wall +1
---
+++ scripts/fedora35/hostname.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eux
+
+if [[ "$PACKER_BUILD_NAME" =~ ^magma-fedora35-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
+  printf "\n127.0.0.1    magma magma.localdomain\n" >> /etc/hosts
+  echo "magma.localdomain" > /etc/hostname
+  hostnamectl set-hostname magma.localdomain
+elif [[ "$PACKER_BUILD_NAME" =~ ^generic-fedora35-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
+  printf "\n127.0.0.1    fedora35 fedora35.localdomain\n" >> /etc/hosts
+  echo "fedora35.localdomain" > /etc/hostname
+  hostnamectl set-hostname fedora35.localdomain
+else
+  printf "\n127.0.0.1    bazinga bazinga.localdomain\n" >> /etc/hosts
+  echo "bazinga.localdomain" > /etc/hostname
+  hostnamectl set-hostname bazinga.localdomain
+fi
