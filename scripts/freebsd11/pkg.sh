9c462ed44ed17e27c5387ed7405eeb91831eb2d9
Ladar Levison <ladar@lavabit.com>
2022-03-07 23:35:25 -0600

    FreeBSD package logic overhauled to restore HTTPS support.

--- scripts/freebsd11/pkg.sh
+++ scripts/freebsd11/pkg.sh
@@ -5,13 +5,37 @@ export FETCH_RETRY=5
 export FETCH_TIMEOUT=30
 export ASSUME_ALWAYS_YES=yes
 
-# Force the use of HTTPS for package updates.
+# We need to use HTTP until the CA bundle has been updated.
 mkdir -p /usr/local/etc/pkg/repos/
-echo 'FreeBSD: { url: "pkg+https://pkg.FreeBSD.org/${ABI}/latest" }' > /usr/local/etc/pkg/repos/FreeBSD.conf
+rm /var/db/pkg/FreeBSD.meta /var/db/pkg/repo-FreeBSD.sqlite
+echo 'FreeBSD: { url: "pkg+http://pkg.FreeBSD.org/${ABI}/latest" }' > /usr/local/etc/pkg/repos/FreeBSD.conf
 
+# Install the packages needed to update the CA bundle.
 pkg bootstrap
-pkg-static update --force
-pkg-static upgrade --yes --force
+pkg-static update -f
+pkg-static install -y perl5 p5-MIME-Base64 p5-Carp curl ca_root_nss
+
+# Download the bundle generator.
+curl --silent --location --output $HOME/mk-ca-bundle.pl https://raw.githubusercontent.com/curl/curl/85f91248cffb22d151d5983c32f0dbf6b1de572a/lib/mk-ca-bundle.pl
+sha256 -c f819e5844935bad3d7eebab566b55066f21bd7138097d2baab7842bd04fd92d2 $HOME/mk-ca-bundle.pl || exit 1
+chmod +x $HOME/mk-ca-bundle.pl
+
+# To ensure we save the certdata.txt file in a predictable place we change directory first.
+cd $HOME && $HOME/mk-ca-bundle.pl $HOME/ca-bundle.crt
+
+# Move the updated bundle to all the places it might be needed.
+cp $HOME/ca-bundle.crt /etc/ssl/cert.pem
+cp $HOME/ca-bundle.crt /usr/local/openssl/cert.pem
+cp $HOME/ca-bundle.crt /usr/local/etc/ssl/cert.pem
+cp $HOME/ca-bundle.crt /usr/local/share/certs/ca-root-nss.crt
+
+# Cleanup the downloaded files and clear the cached repo data.
+rm $HOME/ca-bundle.crt $HOME/certdata.txt $HOME/mk-ca-bundle.pl /var/db/pkg/FreeBSD.meta /var/db/pkg/repo-FreeBSD.sqlite
+
+# Switch to using HTTPS and perform the system upgrade.
+echo 'FreeBSD: { url: "pkg+https://pkg.FreeBSD.org/${ABI}/latest" }' > /usr/local/etc/pkg/repos/FreeBSD.conf
+pkg-static update -f
+pkg-static upgrade --yes
 
 # Generic system utils.
 pkg install --yes vim curl wget sudo bash gnuls gnugrep psmisc
