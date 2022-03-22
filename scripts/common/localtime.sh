716cdbcc1753249c7d86c97fac50caa399b39b12
Ladar Levison <ladar@lavabit.com>
2022-03-22 04:46:23 -0500

    Fix logic error in localtime module.

--- scripts/common/localtime.sh
+++ scripts/common/localtime.sh
@@ -1,8 +1,8 @@
 bash -eux
 
-if [ $(command -v timedatectl) ]; then 
+if [ "$(command -v timedatectl)" ]; then
   timedatectl set-timezone UTC
-elif [ -f /etc/sysconfig/clock ] && [ $(command -v tzdata-update) ]; then
+elif [ -f /etc/sysconfig/clock ] && [ "$(command -v tzdata-update)" ]; then
   printf "ZONE=\"UTC\"\n" > /etc/sysconfig/clock
   tzdata-update
 elif [ -f /etc/localtime ] && [ -f /usr/share/zoneinfo/UTC ]; then
