3b661e1299f18aff0f986623dd8db6144da10e1b
Ladar Levison <ladar@lavabit.com>
2022-03-28 19:15:01 -0500

    Handle localtime being a file.

--- scripts/common/localtime.sh
+++ scripts/common/localtime.sh
@@ -1,10 +1,19 @@
 #!/bin/bash -eux
 
-if [ "$(command -v timedatectl)" ]; then
+# If localtime is a regular file then the timedatectl command will fail.
+if [ -f /etc/localtime ] && [ "$(command -v timedatectl)" ]; then
+  rm -f /etc/localtime
   timedatectl set-timezone UTC
+# Run the timedatectl command without removing a file.
+elif [ "$(command -v timedatectl)" ]; then
+  timedatectl set-timezone UTC
+
+# Handle older distros which use sysconfig and the tzdata-update command.
 elif [ -f /etc/sysconfig/clock ] && [ "$(command -v tzdata-update)" ]; then
   printf "ZONE=\"UTC\"\n" > /etc/sysconfig/clock
   tzdata-update
+
+# Logic of last resort.
 elif [ -f /etc/localtime ] && [ -f /usr/share/zoneinfo/UTC ]; then
   cp -f /usr/share/zoneinfo/UTC /etc/localtime
 elif [ -h /etc/localtime ] && [ -f /usr/share/zoneinfo/UTC ]; then
