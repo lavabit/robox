b9643fc521aed5bfb77ae7728118d89e4be83f5b
Ladar Levison <ladar@lavabit.com>
2022-03-29 19:33:49 -0500

    Updated Magma sripts in older distros so they clone via HTTPS.

--- scripts/debian8/magma.sh
+++ scripts/debian8/magma.sh
@@ -85,7 +85,7 @@ if [ -d magma-develop ]; then
 fi
 
 # Clone the magma repository off Github.
-git clone git://github.com/lavabit/magma.git magma-develop; error
+git clone https://github.com/lavabit/magma.git magma-develop; error
 cd magma-develop; error
 
 # Setup the bin links, just in case we need to troubleshoot things manually.
