6c6b7131a62c5225ea0f0ea088e3511319b509cb
Ladar Levison <ladar@lavabit.com>
2022-05-25 21:43:31 -0500

    Adding initial Alpine 3.16 config.

--- scripts/common/zerodisk.sh
+++ scripts/common/zerodisk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ux
 
-if [[ "$PACKER_BUILD_NAME" =~ ^(generic|magma)-(freebsd1[1-3]|hardenedbsd1[1-3]|openbsd[6-7]|alpine3[5-9]|alpine31[0-5])-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
+if [[ "$PACKER_BUILD_NAME" =~ ^(generic|magma)-(freebsd1[1-3]|hardenedbsd1[1-3]|openbsd[6-7]|alpine3[5-9]|alpine31[0-6])-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
 
   # We fill until full so don't abort on error.
   # set -ux
