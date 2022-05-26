aabbffb44bc7572cd130fa3e8979992cced573a7
Ladar Levison <ladar@lavabit.com>
2022-05-26 15:51:24 -0500

    Fix Alma 9 filename typo.

--- scripts/alma9/dnf.sh
+++ scripts/alma9/dnf.sh
@@ -41,7 +41,7 @@ if [ -f /etc/dnf/plugins/subscription-manager.conf ]; then
 fi
 
 # Alma Repo Setup
-sed -i -e "s/^# baseurl/baseurl/g" /etc/yum.repos.d/almalinu-baseos.repo
+sed -i -e "s/^# baseurl/baseurl/g" /etc/yum.repos.d/almalinux-baseos.repo
 sed -i -e "s/^mirrorlist/#mirrorlist/g" /etc/yum.repos.d/almalinux-baseos.repo
 
 sed -i -e "s/^# baseurl/baseurl/g" /etc/yum.repos.d/almalinux-appstream.repo
