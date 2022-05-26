f1e68b0244861ffb66380ed4fed44035dc34e0f9
Ladar Levison <ladar@lavabit.com>
2022-05-26 18:46:46 -0500

    Redo NetBSD/OpenBSD profile modules. Try and get the escaping right. Closes #244.

--- scripts/netbsd9/profile.sh
+++ scripts/netbsd9/profile.sh
@@ -44,14 +44,14 @@ if [ "\$PS1" ]; then
       elif [ "\${VTE_VERSION:-0}" -ge 3405 ]; then
           PROMPT_COMMAND="__vte_prompt_command"
       else
-          PROMPT_COMMAND='printf "\033]0;%s@%s:%s\007" "\${USER}" "\${HOSTNAME%%.*}" "\${PWD/#\$HOME/~}"'
+          PROMPT_COMMAND='printf "\\033]0;%s@%s:%s\\007" "\${USER}" "\${HOSTNAME%%.*}" "\${PWD/#\$HOME/~}"'
       fi
       ;;
     screen*)
       if [ -e /etc/sysconfig/bash-prompt-screen ]; then
           PROMPT_COMMAND=/etc/sysconfig/bash-prompt-screen
       else
-          PROMPT_COMMAND='printf "\033k%s@%s:%s\033\\" "\${USER}" "\${HOSTNAME%%.*}" "\${PWD/#\$HOME/~}"'
+          PROMPT_COMMAND='printf "\\033k%s@%s:%s\\033\\\\" "\${USER}" "\${HOSTNAME%%.*}" "\${PWD/#\$HOME/~}"'
       fi
       ;;
     *)
@@ -64,12 +64,12 @@ if [ "\$PS1" ]; then
   history -a
   # Turn on checkwinsize
   shopt -s checkwinsize
-  [ "\$PS1" = "\\s-\\v\\\\$ " ] && PS1="[\u@\h \W]\\\$ "
+  [ "\$PS1" = "\\\\s-\\\\v\\\\\\\$ " ] && PS1="[\\u@\\h \\W]\\\\\$ "
   # You might want to have e.g. tty in prompt (e.g. more virtual machines)
   # and console windows
   # If you want to do so, just add e.g.
   # if [ "\$PS1" ]; then
-  #   PS1="[\u@\h:\l \W]\\\$ "
+  #   PS1="[\\u@\\h:\\l \\W]\\\\\$ "
   # fi
   # to your custom modification shell script in /etc/profile.d/ directory
 fi
@@ -93,7 +93,7 @@ if ! shopt -q login_shell ; then # We're not a login shell
     # Current threshold for system reserved uid/gids is 200
     # You could check uidgid reservation validity in
     # /usr/share/doc/setup-*/uidgid file
-    if [ \$UID -gt 199 ] && [ "`/usr/bin/id -gn`" = "`/usr/bin/id -un`" ]; then
+    if [ \$UID -gt 199 ] && [ "\`/usr/bin/id -gn\`" = "\`/usr/bin/id -un\`" ]; then
        umask 002
     else
        umask 022
@@ -198,7 +198,7 @@ if ! shopt -q login_shell ; then # We're not a login shell
     # Current threshold for system reserved uid/gids is 200
     # You could check uidgid reservation validity in
     # /usr/share/doc/setup-*/uidgid file
-    if [ \$UID -gt 199 ] && [ "`/usr/bin/id -gn`" = "`/usr/bin/id -un`" ]; then
+    if [ \$UID -gt 199 ] && [ "\`/usr/bin/id -gn\`" = "\`/usr/bin/id -un\`" ]; then
        umask 002
     else
        umask 022
--- scripts/openbsd6/profile.sh
+++ scripts/openbsd6/profile.sh
@@ -12,23 +12,23 @@ cat <<-EOF > /root/.bash_profile
 # will prevent the need for merging in future updates.
 
 # are we an interactive shell?
-if [ "$PS1" ]; then
-  if [ -z "$PROMPT_COMMAND" ]; then
-    case $TERM in
+if [ "\$PS1" ]; then
+  if [ -z "\$PROMPT_COMMAND" ]; then
+    case \$TERM in
     xterm*|vte*)
       if [ -e /etc/sysconfig/bash-prompt-xterm ]; then
           PROMPT_COMMAND=/etc/sysconfig/bash-prompt-xterm
-      elif [ "${VTE_VERSION:-0}" -ge 3405 ]; then
+      elif [ "\${VTE_VERSION:-0}" -ge 3405 ]; then
           PROMPT_COMMAND="__vte_prompt_command"
       else
-          PROMPT_COMMAND='printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/~}"'
+          PROMPT_COMMAND='printf "\\033]0;%s@%s:%s\\007" "\${USER}" "\${HOSTNAME%%.*}" "\${PWD/#\$HOME/~}"'
       fi
       ;;
     screen*)
       if [ -e /etc/sysconfig/bash-prompt-screen ]; then
           PROMPT_COMMAND=/etc/sysconfig/bash-prompt-screen
       else
-          PROMPT_COMMAND='printf "\033k%s@%s:%s\033\\" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/~}"'
+          PROMPT_COMMAND='printf "\\033k%s@%s:%s\\033\\\\" "\${USER}" "\${HOSTNAME%%.*}" "\${PWD/#\$HOME/~}"'
       fi
       ;;
     *)
@@ -41,12 +41,12 @@ if [ "$PS1" ]; then
   history -a
   # Turn on checkwinsize
   shopt -s checkwinsize
-  [ "$PS1" = "\\s-\\v\\\$ " ] && PS1="[\u@\h \W]\\$ "
+  [ "\$PS1" = "\\\\s-\\\\v\\\\\\\$ " ] && PS1="[\\u@\\h \\W]\\\\\$ "
   # You might want to have e.g. tty in prompt (e.g. more virtual machines)
   # and console windows
   # If you want to do so, just add e.g.
-  # if [ "$PS1" ]; then
-  #   PS1="[\u@\h:\l \W]\\$ "
+  # if [ "\$PS1" ]; then
+  #   PS1="[\\u@\\h:\\l \\W]\\\\\$ "
   # fi
   # to your custom modification shell script in /etc/profile.d/ directory
 fi
@@ -54,14 +54,14 @@ fi
 if ! shopt -q login_shell ; then # We're not a login shell
     # Need to redefine pathmunge, it get's undefined at the end of /etc/profile
     pathmunge () {
-        case ":${PATH}:" in
-            *:"$1":*)
+        case ":\${PATH}:" in
+            *:"\$1":*)
                 ;;
             *)
-                if [ "$2" = "after" ] ; then
-                    PATH=$PATH:$1
+                if [ "\$2" = "after" ] ; then
+                    PATH=\$PATH:\$1
                 else
-                    PATH=$1:$PATH
+                    PATH=\$1:\$PATH
                 fi
         esac
     }
@@ -70,7 +70,7 @@ if ! shopt -q login_shell ; then # We're not a login shell
     # Current threshold for system reserved uid/gids is 200
     # You could check uidgid reservation validity in
     # /usr/share/doc/setup-*/uidgid file
-    if [ $UID -gt 199 ] && [ "`/usr/bin/id -gn`" = "`/usr/bin/id -un`" ]; then
+    if [ \$UID -gt 199 ] && [ "\`/usr/bin/id -gn\`" = "\`/usr/bin/id -un\`" ]; then
        umask 002
     else
        umask 022
@@ -80,11 +80,11 @@ if ! shopt -q login_shell ; then # We're not a login shell
     # Only display echos from profile.d scripts if we are no login shell
     # and interactive - otherwise just process them to set envvars
     for i in /etc/profile.d/*.sh; do
-        if [ -r "$i" ]; then
-            if [ "$PS1" ]; then
-                . "$i"
+        if [ -r "\$i" ]; then
+            if [ "\$PS1" ]; then
+                . "\$i"
             else
-                . "$i" >/dev/null
+                . "\$i" >/dev/null
             fi
         fi
     done
@@ -117,23 +117,23 @@ cat <<-EOF > /home/vagrant/.bash_profile
 # will prevent the need for merging in future updates.
 
 # are we an interactive shell?
-if [ "$PS1" ]; then
-  if [ -z "$PROMPT_COMMAND" ]; then
-    case $TERM in
+if [ "\$PS1" ]; then
+  if [ -z "\$PROMPT_COMMAND" ]; then
+    case \$TERM in
     xterm*|vte*)
       if [ -e /etc/sysconfig/bash-prompt-xterm ]; then
           PROMPT_COMMAND=/etc/sysconfig/bash-prompt-xterm
-      elif [ "${VTE_VERSION:-0}" -ge 3405 ]; then
+      elif [ "\${VTE_VERSION:-0}" -ge 3405 ]; then
           PROMPT_COMMAND="__vte_prompt_command"
       else
-          PROMPT_COMMAND='printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/~}"'
+          PROMPT_COMMAND='printf "\\033]0;%s@%s:%s\\007" "\${USER}" "\${HOSTNAME%%.*}" "\${PWD/#\$HOME/~}"'
       fi
       ;;
     screen*)
       if [ -e /etc/sysconfig/bash-prompt-screen ]; then
           PROMPT_COMMAND=/etc/sysconfig/bash-prompt-screen
       else
-          PROMPT_COMMAND='printf "\033k%s@%s:%s\033\\" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/~}"'
+          PROMPT_COMMAND='printf "\\033k%s@%s:%s\\033\\\\" "\${USER}" "\${HOSTNAME%%.*}" "\${PWD/#\$HOME/~}"'
       fi
       ;;
     *)
@@ -146,12 +146,12 @@ if [ "$PS1" ]; then
   history -a
   # Turn on checkwinsize
   shopt -s checkwinsize
-  [ "$PS1" = "\\s-\\v\\\$ " ] && PS1="[\u@\h \W]\\$ "
+  [ "\$PS1" = "\\\\s-\\\\v\\\\\\\$ " ] && PS1="[\\u@\\h \\W]\\\\\$ "
   # You might want to have e.g. tty in prompt (e.g. more virtual machines)
   # and console windows
   # If you want to do so, just add e.g.
-  # if [ "$PS1" ]; then
-  #   PS1="[\u@\h:\l \W]\\$ "
+  # if [ "\$PS1" ]; then
+  #   PS1="[\\u@\\h:\\l \\W]\\\\\$ "
   # fi
   # to your custom modification shell script in /etc/profile.d/ directory
 fi
@@ -159,14 +159,14 @@ fi
 if ! shopt -q login_shell ; then # We're not a login shell
     # Need to redefine pathmunge, it get's undefined at the end of /etc/profile
     pathmunge () {
-        case ":${PATH}:" in
-            *:"$1":*)
+        case ":\${PATH}:" in
+            *:"\$1":*)
                 ;;
             *)
-                if [ "$2" = "after" ] ; then
-                    PATH=$PATH:$1
+                if [ "\$2" = "after" ] ; then
+                    PATH=\$PATH:\$1
                 else
-                    PATH=$1:$PATH
+                    PATH=\$1:\$PATH
                 fi
         esac
     }
@@ -175,7 +175,7 @@ if ! shopt -q login_shell ; then # We're not a login shell
     # Current threshold for system reserved uid/gids is 200
     # You could check uidgid reservation validity in
     # /usr/share/doc/setup-*/uidgid file
-    if [ $UID -gt 199 ] && [ "`/usr/bin/id -gn`" = "`/usr/bin/id -un`" ]; then
+    if [ \$UID -gt 199 ] && [ "\`/usr/bin/id -gn\`" = "\`/usr/bin/id -un\`" ]; then
        umask 002
     else
        umask 022
@@ -185,11 +185,11 @@ if ! shopt -q login_shell ; then # We're not a login shell
     # Only display echos from profile.d scripts if we are no login shell
     # and interactive - otherwise just process them to set envvars
     for i in /etc/profile.d/*.sh; do
-        if [ -r "$i" ]; then
-            if [ "$PS1" ]; then
-                . "$i"
+        if [ -r "\$i" ]; then
+            if [ "\$PS1" ]; then
+                . "\$i"
             else
-                . "$i" >/dev/null
+                . "\$i" >/dev/null
             fi
         fi
     done
