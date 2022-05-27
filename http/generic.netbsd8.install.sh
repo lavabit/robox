287c44830620ff6a078c29d521990a29e95a10cb
Ladar Levison <ladar@lavabit.com>
2022-05-27 09:21:52 -0500

    Prefer IPv4 on NetBSD to avoid bug which causes NetBSD to prefer invalid IPv6 routes.

--- http/generic.netbsd8.install.sh
+++ http/generic.netbsd8.install.sh
@@ -12,3 +12,13 @@ sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /mnt/etc/ssh
 printf "sshd=YES\n" >> /mnt/etc/rc.conf
 printf "dhcpcd=YES\n" >> /mnt/etc/rc.conf
 printf "ntpdate=YES\n" >> /mnt/etc/rc.conf
+
+# On networks without IPv6 support, NetBSD guests will autoconfigure an invalid route, and try to use
+# the invalid route, before timing out and falling back to IPv4. These settings mitigate the performance
+# impact.
+# https://www.netbsd.org/docs/guide/en/netbsd.html#chap-virt-guest-ipv6
+# https://web.archive.org/web/20211223010333/http://www.netbsd.org/docs/guide/en/netbsd.html#chap-virt-guest-ipv6
+printf "ip6addrctl=YES\n" >> /mnt/etc/rc.conf
+printf "ip6addrctl_policy=\"ipv4_prefer\"\n" >> /mnt/etc/rc.conf
+
+
