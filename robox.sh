8bd14edb2512dbce204b54cf55361032c8c1bc89
Ladar Levison <ladar@lavabit.com>
2022-06-08 12:42:25 -0500

    Added comment about removing the exception when we update the guest additions URL.

--- robox.sh
+++ robox.sh
@@ -956,9 +956,12 @@ function links() {
 
   # Detect downloads that aren't being fetched by the packer-cache.json file.
   for ((i = 0; i < ${#ISOURLS[@]}; ++i)); do
-    grep --silent "${ISOURLS[$i]}" packer-cache.json | \
-      grep -v "https://www.virtualbox.org/download/testcase/VBoxGuestAdditions_6.1.35-151478.iso" || \
-      echo "Cache Failure:  ${ISOURLS[$i]}"
+    grep --silent "${ISOURLS[$i]}" packer-cache.json || \
+      echo "Cache Failure:  ${ISOURLS[$i]}" | \
+      grep -v "https://www.virtualbox.org/download/testcase/VBoxGuestAdditions_6.1.35-151478.iso"
+      #####6
+       # Remove the above exception when the v6.1.36 guest additions are released.
+      ####
   done
   
   # Check whether the VIrtualBox v6.1.36 guest additions have been released, so we can switch the RHEL 9 (and friends)
