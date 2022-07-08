9e4cde6d541b1629c830ff3bf6e5fd9cf9b4b210
Ladar Levison <ladar@lavabit.com>
2022-07-08 02:05:21 -0500

    Fix inconsistent repo naming on OpenSUSE.

--- scripts/opensuse15/zypper.sh
+++ scripts/opensuse15/zypper.sh
@@ -12,12 +12,12 @@ zypper --non-interactive removerepo "`zypper repos 1 | head -1 | awk -F':' '{pri
 # Add the default repositories for this release.
 # zypper --non-interactive addrepo https://download.opensuse.org/distribution/leap/${version}/repo/oss/ openSUSE-Leap-${version}-Oss
 # zypper --non-interactive addrepo https://download.opensuse.org/distribution/leap/${version}/repo/non-oss/ openSUSE-Leap-${version}-Non-Oss
-# zypper --non-interactive addrepo https://download.opensuse.org/update/leap/${version}/oss/ openSUSE-Leap-${version}-Update
+# zypper --non-interactive addrepo https://download.opensuse.org/update/leap/${version}/oss/ openSUSE-Leap-${version}-Update-Oss
 # zypper --non-interactive addrepo https://download.opensuse.org/update/leap/${version}/non-oss/ openSUSE-Leap-${version}-Update-Non-Oss
 
 zypper --non-interactive addrepo https://mirrors.edge.kernel.org/opensuse/distribution/leap/${version}/repo/oss/ openSUSE-Leap-${version}-Oss
 zypper --non-interactive addrepo https://mirrors.edge.kernel.org/opensuse/distribution/leap/${version}/repo/non-oss/ openSUSE-Leap-${version}-Non-Oss
-zypper --non-interactive addrepo https://mirrors.edge.kernel.org/opensuse/update/leap/${version}/oss/ openSUSE-Leap-${version}-Update
+zypper --non-interactive addrepo https://mirrors.edge.kernel.org/opensuse/update/leap/${version}/oss/ openSUSE-Leap-${version}-Update-Oss
 zypper --non-interactive addrepo https://mirrors.edge.kernel.org/opensuse/update/leap/${version}/non-oss/ openSUSE-Leap-${version}-Update-Non-Oss
 
 # Clean out any stale cache data.
--- scripts/opensuse42/zypper.sh
+++ scripts/opensuse42/zypper.sh
@@ -12,12 +12,12 @@ zypper --non-interactive removerepo "openSUSE-Leap-${version}-0"
 # Add the default repositories for this release.
 # zypper --non-interactive addrepo https://download.opensuse.org/distribution/leap/${version}/repo/oss/ openSUSE-Leap-${version}-Oss
 # zypper --non-interactive addrepo https://download.opensuse.org/distribution/leap/${version}/repo/non-oss/ openSUSE-Leap-${version}-Non-Oss
-# zypper --non-interactive addrepo https://download.opensuse.org/update/leap/${version}/oss/ openSUSE-Leap-${version}-Update
+# zypper --non-interactive addrepo https://download.opensuse.org/update/leap/${version}/oss/ openSUSE-Leap-${version}-Update-Oss
 # zypper --non-interactive addrepo https://download.opensuse.org/update/leap/${version}/non-oss/ openSUSE-Leap-${version}-Update-Non-Oss
 
 zypper --non-interactive addrepo https://ftp5.gwdg.de/pub/opensuse/discontinued/distribution/leap/${version}/repo/oss/ openSUSE-Leap-${version}-Oss
 zypper --non-interactive addrepo https://ftp5.gwdg.de/pub/opensuse/discontinued/distribution/leap/${version}/repo/non-oss/ openSUSE-Leap-${version}-Non-Oss
-zypper --non-interactive addrepo https://ftp5.gwdg.de/pub/opensuse/discontinued/update/leap/${version}/oss/ openSUSE-Leap-${version}-Update
+zypper --non-interactive addrepo https://ftp5.gwdg.de/pub/opensuse/discontinued/update/leap/${version}/oss/ openSUSE-Leap-${version}-Update-Oss
 zypper --non-interactive addrepo https://ftp5.gwdg.de/pub/opensuse/discontinued/update/leap/${version}/non-oss/ openSUSE-Leap-${version}-Update-Non-Oss
 
 # Clean out any stale cache data.
