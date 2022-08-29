d98cd6c30ccbb085c84323233c7e3170f5c29a81
Ladar Levison <ladar@lavabit.com>
2022-08-29 18:08:48 -0500

    Fix up dnf modules.

--- scripts/alma8/dnf.sh
+++ scripts/alma8/dnf.sh
@@ -41,16 +41,13 @@ if [ -f /etc/dnf/plugins/subscription-manager.conf ]; then
 fi
 
 # Alma Repo Setup
-sed -i -e "s/^# baseurl/baseurl/g" /etc/yum.repos.d/almalinux.repo
+sed -i -e "s/^#[ ]\?baseurl/baseurl/g" /etc/yum.repos.d/almalinux.repo
 sed -i -e "s/^mirrorlist/#mirrorlist/g" /etc/yum.repos.d/almalinux.repo
 
-sed -i -e "s/^# baseurl/baseurl/g" /etc/yum.repos.d/almalinux-powertools.repo
+sed -i -e "s/^#[ ]\?baseurl/baseurl/g" /etc/yum.repos.d/almalinux-powertools.repo
 sed -i -e "s/^mirrorlist/#mirrorlist/g" /etc/yum.repos.d/almalinux-powertools.repo
 
-sed -i -e "s/^# baseurl/baseurl/g" /etc/yum.repos.d/almalinux-extras.repo
-sed -i -e "s/^mirrorlist/#mirrorlist/g" /etc/yum.repos.d/almalinux-extras.repo
-
-sed -i -e "s/^# baseurl/baseurl/g" /etc/yum.repos.d/almalinux-plus.repo
+sed -i -e "s/^#[ ]\?baseurl/baseurl/g" /etc/yum.repos.d/almalinux-plus.repo
 sed -i -e "s/^mirrorlist/#mirrorlist/g" /etc/yum.repos.d/almalinux-plus.repo
 
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-AlmaLinux
