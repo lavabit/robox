ea267e3bafd4638bdaf62d9e5c4275035d3e41c4
Ladar Levison <ladar@lavabit.com>
2022-09-01 07:41:30 -0500

    Removed NetBSD 9 from the exception list.

--- robox.sh
+++ robox.sh
@@ -101,7 +101,7 @@ TAGS="$GENERIC_TAGS $GENERIC_X32_TAGS $ROBOX_TAGS $ROBOX_X32_TAGS $MAGMA_TAGS $L
 FILTERED_TAGS="lavabit/magma-alpine lavabit/magma-arch lavabit/magma-freebsd lavabit/magma-gentoo lavabit/magma-openbsd"
 
 # A list of configs to skip during complete build operations.
-export EXCEPTIONS="generic-netbsd9-libvirt"
+export EXCEPTIONS=""
 
 # The repository URLs, so we can catch any which might disappeared since the last build.
 # Ubuntu 16.04
