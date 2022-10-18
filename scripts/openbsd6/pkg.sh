024355ee507bb77599e9802548f7b850c1a3fc09
Ladar Levison <ladar@lavabit.com>
2022-10-18 17:21:39 -0500

    Fixed the OpenBSD 6.9 TLS problem.

--- scripts/openbsd6/pkg.sh
+++ scripts/openbsd6/pkg.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 
-# Setup a the package mirror.
+# Setup an HTTP package mirror so we can download cURL.
+echo "http://mirrors.lavabit.com/openbsd/" > /etc/installurl
+
+# Install cURL.
+pkg_add -I curl
+
+# The certificate authority file needs to be updated so that
+# HTTPS can be used to download updates/packages. We start by 
+# downloading the helper script. If the GitHub certificate fails, 
+# add the "--insecure" flag to overcome the it. We check the downloaded
+# file hash, so it should be safe.
+curl --silent --location --output $HOME/mk-ca-bundle.pl https://raw.githubusercontent.com/curl/curl/85f91248cffb22d151d5983c32f0dbf6b1de572a/lib/mk-ca-bundle.pl
+echo "SHA256 ($HOME/mk-ca-bundle.pl) = f819e5844935bad3d7eebab566b55066f21bd7138097d2baab7842bd04fd92d2" | sha256 -c || exit 1
+chmod +x $HOME/mk-ca-bundle.pl
+
+# To ensure we save the certdata.txt file in a predictable place we change directories first.
+cd $HOME && $HOME/mk-ca-bundle.pl $HOME/ca-bundle.crt
+cp $HOME/ca-bundle.crt /etc/ssl/cert.pem
+
+# Cleanup the files used to update the CA file and then switch to using HTTPS for package downloads.
+rm $HOME/ca-bundle.crt $HOME/certdata.txt $HOME/mk-ca-bundle.pl
 echo "https://mirrors.lavabit.com/openbsd/" > /etc/installurl
 
 # Update the system.
