3acdc24b0e8480f3154b9b80eb20abc0da76248f
Ladar Levison <ladar@lavabit.com>
2022-11-06 21:29:38 -0600

    Fix Parallels RHEL v8 builds.

--- scripts/rocky8/lvm.sh
+++ scripts/rocky8/lvm.sh
@@ -3,6 +3,5 @@
 VGID=$(vgdisplay --columns --noheadings --options vg_name)
 PVID=$(vgdisplay --columns --noheadings --options pv_name $VGID)
 LVID=$(df --output=source / | tail -1)
-lvextend $LVID $PVID --resizefs 
-
+lvextend $LVID $PVID --resizefs || df -h
 
