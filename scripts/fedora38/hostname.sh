70c1f38536eb237a6d2b180cc305aab49be48e6d
JuliusLongmind <5893405+JuliusLongmind@users.noreply.github.com>
2023-07-27 21:22:14 +0200

    Fedora 38 has been added to the libvirt provider

---
+++ scripts/fedora38/hostname.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eux
+
+if [[ "$PACKER_BUILD_NAME" =~ ^magma-fedora38-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
+  printf "\n127.0.0.1    magma magma.localdomain\n" >> /etc/hosts
+  echo "magma.localdomain" > /etc/hostname
+  hostnamectl set-hostname magma.localdomain
+elif [[ "$PACKER_BUILD_NAME" =~ ^generic-fedora38-(vmware|hyperv|libvirt|parallels|virtualbox)$ ]]; then
+  printf "\n127.0.0.1    fedora38 fedora38.localdomain\n" >> /etc/hosts
+  echo "fedora38.localdomain" > /etc/hostname
+  hostnamectl set-hostname fedora38.localdomain
+else
+  printf "\n127.0.0.1    bazinga bazinga.localdomain\n" >> /etc/hosts
+  echo "bazinga.localdomain" > /etc/hostname
+  hostnamectl set-hostname bazinga.localdomain
+fi
---
+++ scripts/fedora38/updatedb.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -eux
+
+retry() {
+  local COUNT=1
+  local DELAY=0
+  local RESULT=0
+  while [[ "${COUNT}" -le 10 ]]; do
+    [[ "${RESULT}" -ne 0 ]] && {
+      [ "`which tput 2> /dev/null`" != "" ] && [ -n "$TERM" ] && tput setaf 1
+      echo -e "\n${*} failed... retrying ${COUNT} of 10.\n" >&2
+      [ "`which tput 2> /dev/null`" != "" ] && [ -n "$TERM" ] && tput sgr0
+    }
+    "${@}" && { RESULT=0 && break; } || RESULT="${?}"
+    COUNT="$((COUNT + 1))"
+
+    # Increase the delay with each iteration.
+    DELAY="$((DELAY + 10))"
+    sleep $DELAY
+  done
+
+  [[ "${COUNT}" -gt 10 ]] && {
+    [ "`which tput 2> /dev/null`" != "" ] && [ -n "$TERM" ] && tput setaf 1
+    echo -e "\nThe command failed 10 times.\n" >&2
+    [ "`which tput 2> /dev/null`" != "" ] && [ -n "$TERM" ] && tput sgr0
+  }
+
+  return "${RESULT}"
+}
+
+retry dnf --assumeyes install plocate
+
+# Override the default plocate timer so it runs every hour.
+if [ ! -d /etc/systemd/system/plocate-updatedb.timer.d/ ]; then
+  mkdir --parents /etc/systemd/system/plocate-updatedb.timer.d/
+fi
+
+chcon system_u:object_r:systemd_unit_file_t:s0 /etc/systemd/system/plocate-updatedb.timer.d/
+chmod 755 /etc/systemd/system/plocate-updatedb.timer.d/
+
+cat <<-EOF > /etc/systemd/system/plocate-updatedb.timer.d/override.conf
+[Unit]
+Description=Updates plocate database every hour
+
+[Timer]
+OnCalendar=hourly
+AccuracySec=1h
+EOF
+
+chcon system_u:object_r:systemd_unit_file_t:s0 /etc/systemd/system/plocate-updatedb.timer.d/override.conf
+chmod 644 /etc/systemd/system/plocate-updatedb.timer.d/override.conf
+
+# Force systemd to load the new unit file, and ensure the timer is enabled.
+systemctl daemon-reload && systemctl enable plocate-updatedb.timer
