cb9d34bc382437d7ff023c0c26a3a3c2b4ef03d6
Ladar Levison <ladar@lavabit.com>
2023-09-06 19:21:02 -0500

    Fixed minor oversights with the new box configs.

---
+++ scripts/ubuntu2304/mysql.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -ex
+
+# If the TERM environment variable is set to dumb, tput will generate spurrious error messages.
+[ "$TERM" == "dumb" ] && export TERM="vt100"
+
+retry() {
+  local COUNT=1
+  local DELAY=0
+  local RESULT=0
+  while [[ "${COUNT}" -le 10 ]]; do
+    [[ "${RESULT}" -ne 0 ]] && {
+      [ "`which tput 2> /dev/null`" != "" ] && [ -n "$TERM" ] && tput setaf 1
+      echo -e "\n${*} failed... retrying ${COUNT} of 10.\n" >&2
+      [ "`which tput 2> /dev/null`" != "" ] && [ -n "$TERM" ] && tput sgr0
+    }
+    "${@}" && { RESULT=0 && break; } || RESULT="${?}"
+    COUNT="$((COUNT + 1))"
+
+    # Increase the delay with each iteration.
+    DELAY="$((DELAY + 10))"
+    sleep $DELAY
+  done
+
+  [[ "${COUNT}" -gt 10 ]] && {
+    [ "`which tput 2> /dev/null`" != "" ] && [ -n "$TERM" ] && tput setaf 1
+    echo -e "\nThe command failed 10 times.\n" >&2
+    [ "`which tput 2> /dev/null`" != "" ] && [ -n "$TERM" ] && tput sgr0
+  }
+
+  return "${RESULT}"
+}
+
+# To allow for automated installs, we disable interactive configuration steps.
+export DEBIAN_FRONTEND=noninteractive
+export DEBCONF_NONINTERACTIVE_SEEN=true
+
+# The mysql client and related utilities.
+retry apt-get --assume-yes install mysql-client mysql-server perl libdbi-perl libmysqlclient21 mysql-common libdbd-mysql-perl
+
+# Enable mysql and configure it to automatically start.
+systemctl start mysql.service && systemctl enable mysql.service
+
+# Setup the mysql root account with a random password.
+export PRAND=`openssl rand -base64 18`
+mysqladmin --user=root password "$PRAND"
+
+# Allow the root user to login to mysql as root by saving the randomly generated password.
+printf "\n\n[mysql]\nuser=root\npassword=$PRAND\n\n" >> /root/.my.cnf
