d37f9751c8e3dc524d411036e8441d7fc74d8933
Ladar Levison <ladar@lavabit.com>
2023-10-02 03:14:40 -0500

    Use short opts in machine module so it works on BSDs.

--- scripts/common/machine.sh
+++ scripts/common/machine.sh
@@ -3,15 +3,15 @@
 # Delete the machine-id file so a new value gets generated during subsequent reboots.
 
 if [ -f /var/lib/dbus/machine-id ]; then
-  truncate --size=0 /var/lib/dbus/machine-id
+  truncate -s 0 /var/lib/dbus/machine-id
 fi
 
 if [ -f /etc/machine-id ]; then
-  truncate --size=0 /etc/machine-id
+  truncate -s 0 /etc/machine-id
 fi
 
 if [ -f /run/machine-id ]; then
-  truncate --size=0 /run/machine-id
+  truncate -s 0 /run/machine-id
 fi
 
 # printf "@reboot root command bash -c '/usr/bin/systemd-machine-id-setup ; rm --force /etc/cron.d/machine-id'\n" > /etc/cron.d/machine-id
