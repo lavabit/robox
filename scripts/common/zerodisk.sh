f440a7422c6d01c50bac825b9854eb142f05a492
Ladar Levison <ladar@lavabit.com>
2023-10-31 19:47:05 -0500

    Speed up the zerodisk by using a larger block size.

--- scripts/common/zerodisk.sh
+++ scripts/common/zerodisk.sh
@@ -6,21 +6,21 @@ if [[ "$PACKER_BUILD_NAME" =~ ^(generic|magma)-(freebsd1[1-4]|hardenedbsd|harden
   # set -ux
 
   # Whiteout root
-  dd if=/dev/zero of=/zerofill bs=1K
+  dd if=/dev/zero of=/zerofill bs=1M
   sync -f /zerofill
   rm -f /zerofill
 
   # Whiteout /boot
   if [ -d "/boot" ]; then
-    dd if=/dev/zero of=/boot/zerofill bs=1K
+    dd if=/dev/zero of=/boot/zerofill bs=1M
     sync -f /boot/zerofill
     rm -f /boot/zerofill
   fi
 
 elif [[ "$PACKER_BUILD_NAME" =~ ^(generic|magma)-(netbsd[8-9])-(vmware|hyperv|libvirt|parallels|virtualbox)-(x64|x32|a64|a32|p64|p32|m64|m32)$ ]]; then
 
-  # Whiteout root
-  dd if=/dev/zero of=/zerofill bs=8192
+  # Whiteout root.
+  dd if=/dev/zero of=/zerofill bs=1m msgfmt=human
   sync -f /zerofill
   rm -f /zerofill
 
@@ -36,23 +36,23 @@ elif [[ "$PACKER_BUILD_NAME" =~ ^(generic|magma)-(dragonflybsd[5-6]?)-(vmware|hy
 else
 
   # Whiteout the root partition.
-  rootcount=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
+  rootcount=$(df --sync -mP / | tail -n1  | awk -F ' ' '{print $4}')
   rootcount=$(($rootcount/4))
-  ( dd if=/dev/zero of=/zerofill_1 bs=1K count=$rootcount || echo "dd exit code $? suppressed" ) &
-  ( dd if=/dev/zero of=/zerofill_2 bs=1K count=$rootcount || echo "dd exit code $? suppressed" ) &
-  ( dd if=/dev/zero of=/zerofill_3 bs=1K count=$rootcount || echo "dd exit code $? suppressed" ) &
-  ( dd if=/dev/zero of=/zerofill_4 bs=1K count=$rootcount || echo "dd exit code $? suppressed" ) &
+  ( dd if=/dev/zero of=/zerofill_1 bs=1M count=$rootcount || echo "dd exit code $? suppressed" ) &
+  ( dd if=/dev/zero of=/zerofill_2 bs=1M count=$rootcount || echo "dd exit code $? suppressed" ) &
+  ( dd if=/dev/zero of=/zerofill_3 bs=1M count=$rootcount || echo "dd exit code $? suppressed" ) &
+  ( dd if=/dev/zero of=/zerofill_4 bs=1M count=$rootcount || echo "dd exit code $? suppressed" ) &
   wait ; sync || echo "sync exit code $? suppressed"
   rm --force /zerofill_1 /zerofill_2 /zerofill_3 /zerofill_4
 
   # Whiteout boot if the block count is different then root, otherwise if the
   # block counts are identical, we assume both folders are on the same partition.
-  rootcount=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
+  rootcount=$(df --sync -mP / | tail -n1  | awk -F ' ' '{print $4}')
   rootcount=$(($rootcount-1))
-  bootcount=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
+  bootcount=$(df --sync -mP /boot | tail -n1 | awk -F ' ' '{print $4}')
   bootcount=$(($bootcount-1))
   if [ $rootcount != $bootcount ]; then
-    dd if=/dev/zero of=/boot/zerofill bs=1K count=$bootcount || echo "dd exit code $? suppressed"
+    dd if=/dev/zero of=/boot/zerofill bs=1M count=$bootcount || echo "dd exit code $? suppressed"
     sync || echo "sync exit code $? suppressed"
     rm --force /boot/zerofill
   fi
@@ -68,7 +68,7 @@ else
   if [ "x${swapuuid}" != "x" ]; then
     swappart="`readlink -f /dev/disk/by-uuid/$swapuuid`"
     /sbin/swapoff "$swappart"
-    dd if=/dev/zero of="$swappart" bs=1K || echo "dd exit code $? suppressed"
+    dd if=/dev/zero of="$swappart" bs=1M || echo "dd exit code $? suppressed"
     /sbin/mkswap -U "$swapuuid" "$swappart"
   fi
 
